1d114c709415a4a4af1d99407fd9fdaceda10783
Dmitry Murashov <d.murashov@geoscan.aero>
2020-10-23 15:56:57 +0300

    Versioning: restructured content of http://<host>/info as JSON array

--- components/http/pages/version.cpp
+++ components/http/pages/version.cpp
@@ -8,16 +8,20 @@
 esp_err_t infoPageHandler(httpd_req_t *req)
 {
 	std::string response;
-	response.append("ESP32 Firmware Version: ");
+
+	response.append("[");
+	response.append("\"");
 	response.append(ESP32_FIRMWARE_VERSION);
-	response.append("\n");
+	response.append("\"");
 
 	std::string stm32ver;
 	if (versionStmGet(stm32ver)) {
-		response.append("STM32 Firmware Version: ");
+		response.append(", ");
+		response.append("\"");
 		response.append(stm32ver);
-		response.append("\n");
+		response.append("\"");
 	}
+	response.append("]");
 
 	esp_err_t res = httpd_resp_send(req, response.data(), response.size());
 
