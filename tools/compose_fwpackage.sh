7b161ce685940089931484bc02b1b4ee72a728b8
Dmitry Murashov <d.murashov@geoscan.aero>
2020-11-05 12:48:37 +0300

    Tools: enhanced automatic fw archive composer

--- tools/compose_fwpackage.sh
+++ tools/compose_fwpackage.sh
@@ -9,7 +9,11 @@ name=$prefix-`date +%Y-%m-%d-%H-%M`
 rm -r -f *bin *properties *zip ./$prefix*
 
 ./extract_binaries.sh
-wget -O par.properties https://pioneer-doc.readthedocs.io/ru/master/_downloads/eced3253338b09e60e628c48571a617c/Pioneer-mini-release-1.0.0003.properties
+# wget -O par.properties https://pioneer-doc.readthedocs.io/ru/master/_downloads/eced3253338b09e60e628c48571a617c/Pioneer-mini-release-1.0.0003.properties
+wget -O $properties https://pioneer-doc.readthedocs.io/ru/master/_downloads/9f141dd22681fe536e57ce7bd4043e9d/Pioneer-mini-esp-test-1.0.0006.properties
+
+sed -i 's/Copter_landedState=1.0/Copter_landedState=0.0/g' $properties
+
 zip $name.zip *bin $properties
 
 rm *bin *properties
