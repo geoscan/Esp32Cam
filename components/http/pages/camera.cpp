2c8c6566ba3b2a9fdd7fd8e45d45a9504118a7e1
Dmitry Murashov <d.murashov@geoscan.aero>
2021-02-16 13:49:07 +0300

    [http: camera] Implemented and tested the interface, works well. However, an attempt to perform a command in an absence of SD card leads to longish "hanging"

--- components/http/pages/camera.cpp
+++ components/http/pages/camera.cpp
@@ -9,6 +9,8 @@
 #include <utility>
 #include <string>
 #include <cJSON.h>
+#include <sdkconfig.h>
+#include <Ov2640.hpp>
 
 using namespace std;
 
@@ -24,8 +26,19 @@ static constexpr const char *kStop    = "stop";     // value
 static constexpr const char *kStart   = "start";    // value
 // JSON response
 static constexpr const char *kSuccess = "success";
+static constexpr const char *kMessage = "message";
 
-static constexpr esp_err_t kEspErrorNone = ESP_FAIL - 1;  // Workaround to provide 3-state bool for printStatus(2)
+enum Error : esp_err_t {
+	// Standard ESP's errors
+	Ok      = ESP_OK,
+	Err     = ESP_FAIL,
+	ErrArg  = ESP_ERR_INVALID_ARG,
+
+	// Custom errors
+	ErrNone = ESP_FAIL - 1,
+	ErrCam  = ESP_FAIL - 2,
+	ErrSd   = ESP_FAIL - 3,
+};
 
 static string getArgValueByKey(httpd_req_t *req, const char *key)
 {
@@ -40,7 +53,7 @@ static string getArgValueByKey(httpd_req_t *req, const char *key)
 	char urlQuery[urlQueryLength] = {0};
 
 	httpd_req_get_url_query_str(req, urlQuery, urlQueryLength);
-	if (httpd_query_key_value(urlQuery, key, valueBuf, sizeof(valueBuf)) == ESP_OK)
+	if (httpd_query_key_value(urlQuery, key, valueBuf, sizeof(valueBuf)) == Ok)
 	{
 		return valueBuf;
 	}
@@ -48,57 +61,105 @@ static string getArgValueByKey(httpd_req_t *req, const char *key)
 	return {};
 }
 
-static esp_err_t processVideoStream(httpd_req_t *req)
+static Error processVideoStream()
 {
-	return ESP_OK;
+	return Err;
 }
 
-static esp_err_t processVideoRecord(httpd_req_t *req)
+static Error processVideoRecord()
 {
-	return ESP_OK;
+	return Err;
 }
 
-static esp_err_t processPhoto(httpd_req_t *req)
+static Error processPhoto(string name)
 {
-	return ESP_OK;
+	static constexpr const char *kJpg = ".jpg";
+
+	if (name.length() == 0) {
+		return ErrArg;
+	}
+
+	// <name> -> <mount_point>/<name>.jpg
+	name.insert(0, CONFIG_SD_FAT_MOUNT_POINT"/");
+	name.append(kJpg);
+
+	// Try to open file
+	auto *file = fopen(name.c_str(), "wb");
+	if (file == NULL) {
+		return ErrSd;
+	}
+
+	// Try to capture an image
+	auto image = Ov2640::instance().jpeg();
+	if (!image) {
+		return ErrCam;
+	}
+
+	Error ret = Ok;
+	// Try to write
+	if (fwrite(image->data().data(), 1, image->data().size(), file) != image->data().size()) {
+		ret = ErrSd;
+	}
+	fclose(file);
+
+	return ret;
 }
 
-static esp_err_t printStatus(httpd_req_t *req, esp_err_t resp)
+static esp_err_t printStatus(httpd_req_t *req, esp_err_t res)
 {
-	esp_err_t res = kEspErrorNone;
 	auto *root = cJSON_CreateObject();
 
 	cJSON_AddItemToObject(root, kVideoStream, cJSON_CreateFalse());
 	cJSON_AddItemToObject(root, kVideoRecord, cJSON_CreateFalse());
 
-	if (resp != kEspErrorNone) {
-		cJSON_AddItemToObject(root, kSuccess, cJSON_CreateBool((int)(resp == ESP_OK)));
+	if (res != ErrNone) {
+		cJSON_AddItemToObject(root, kSuccess, cJSON_CreateBool((int)(res == Ok)));
+		if (res != Ok) {
+			switch (res) {
+
+				case ErrCam:
+					cJSON_AddItemReferenceToObject(root, kMessage, cJSON_CreateString("Camera Error"));
+					break;
+
+				case ErrSd:
+					cJSON_AddItemReferenceToObject(root, kMessage, cJSON_CreateString("Storage or filesystem error"));
+					break;
+
+				case ErrArg:
+					cJSON_AddItemReferenceToObject(root, kMessage, cJSON_CreateString("Wrong input argument(s)"));
+					break;
+
+				default:
+					cJSON_AddItemReferenceToObject(root, kMessage, cJSON_CreateString("Unknown error"));
+					break;
+			}
+		}
 	}
 
 	char *json = cJSON_Print(root);
 
 	httpd_resp_set_type(req, "application/json");
-	res = httpd_resp_send(req, json, strlen(json));
+	res = httpd_resp_send(req, json, strlen(json)) == ESP_OK ? Ok : Err;
 
 	free(json);
 	cJSON_Delete(root);
 
-	return res;
+	return static_cast<esp_err_t>(res);
 }
 
 extern "C" esp_err_t cameraHandler(httpd_req_t *req)
 {
-	esp_err_t ret = kEspErrorNone;
+	Error ret = ErrNone;
 
 	auto value = getArgValueByKey(req, kFunction);
 
 	if (value.length() != 0) {
 		if (value == kVideoStream) {
-			ret = processVideoStream(req);
+			ret = processVideoStream();
 		} else if (value == kVideoRecord) {
-			ret = processVideoRecord(req);
+			ret = processVideoRecord();
 		} else if (value == kPhoto) {
-			ret = processPhoto(req);
+			ret = processPhoto(getArgValueByKey(req, kName));
 		}
 	}
 
