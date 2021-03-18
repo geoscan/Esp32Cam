db437f30c8eb82cee5e67f4f88cbaa38caccb576
Dmitry Murashov <d.murashov@geoscan.aero>
2021-03-18 11:13:49 +0300

    [camera_streamer] Rolled back to use of std::shared_ptr while retaining performance level

--- components/camera_streamer/CameraStream.cpp
+++ components/camera_streamer/CameraStream.cpp
@@ -28,7 +28,7 @@ void CameraStream::operator()()
 	using Time = decltype(Utility::bootTimeUs());
 	static const auto kWaitMs = (fps > 0) ? 1000 / fps : 0;
 
-	auto img = Ov2640::instance().jpeg(); // Trigger HW-initialization
+	std::shared_ptr<Ov2640::Image> img = Ov2640::instance().jpeg(); // Trigger HW-initialization
 
 	Time lastSend = 0;
 
@@ -37,7 +37,7 @@ void CameraStream::operator()()
 			lastSend = Utility::bootTimeUs() / 1000;
 		}
 
-		key.notify(*img.get());
+		key.notify(img);
 		img = Ov2640::instance().jpeg();
 
 		if (fps > 0) {
--- components/camera_streamer/FrameSender.hpp
+++ components/camera_streamer/FrameSender.hpp
@@ -17,7 +17,7 @@ namespace CameraStreamer {
 class FrameSender {
 public:
 	FrameSender(asio::ip::udp::socket &);
-	void processFrame(Ov2640::Image &);
+	void processFrame(const std::shared_ptr<Ov2640::Image> &);
 	void processTcpConnected(asio::ip::address, unsigned short);
 	void processTcpDisconnected(asio::ip::address);
 private:
