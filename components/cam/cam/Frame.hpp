85a4195860e6e94de704e2255f17a0719fe8cb92
Dmitry Murashov <d.murashov@geoscan.aero>
2021-03-30 13:47:20 +0300

    [cam, ov2640] Implemented width() and height() methods for camera frame

---
+++ components/cam/cam/Frame.hpp
@@ -0,0 +1,24 @@
+//
+// Frame.hpp
+//
+// Created on: Mar 30, 2021
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#ifndef CAM_CAM_FRAME_HPP
+#define CAM_CAM_FRAME_HPP
+
+#include "utility/Buffer.hpp"
+
+namespace Cam {
+
+struct Frame : Utility::Buffer {
+	using Utility::Buffer::Buffer;
+
+	virtual int width();
+	virtual int height();
+};
+
+}  // namespace Cam
+
+#endif  // CAM_CAM_FRAME_HPP
