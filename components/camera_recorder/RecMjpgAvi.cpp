4157c2c5e9c2a7e3d4356522fe84ec9ea00c5353
Dmitry Murashov <d.murashov@geoscan.aero>
2021-03-30 14:24:27 +0300

    [camera_recorder] mjpg/avi recorder
    [camera_recorder] Thread management fixes, minor refactoring

--- components/camera_recorder/RecMjpgAvi.cpp
+++ components/camera_recorder/RecMjpgAvi.cpp
@@ -5,29 +5,44 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
-#include "camera_recorder/RecMjpgAvi.h"
+#include "camera_recorder/RecMjpgAvi.hpp"
 #include "utility/time.hpp"
 
 using namespace CameraRecorder;
 
 void RecMjpgAvi::updateFps()
 {
+	if (stat.frames < kFrameRegCount) {
+		++stat.frames;
+	}
+}
 
+void RecMjpgAvi::calculateFps()
+{
+	if (std::isnan(stat.fps)) {
+		float frames = static_cast<float>(stat.frames);
+		std::chrono::duration<float> duration = std::chrono::microseconds(Utility::bootTimeUs()) - stat.started;
+		stat.fps = frames / duration.count();
+	}
 }
 
 void RecMjpgAvi::onNewFrame(Key::Type aFrame)
 {
-	frame = aFrame;
-	updateFps();
+	if (stat.running) {
+		frame = aFrame;
+		stat.width = aFrame->width();
+		stat.height = aFrame->height();
+	}
 }
 
 bool RecMjpgAvi::start(const char *aFilename)
 {
-	if (stat.fd = AVI_open_output_file(const_cast<char *>(aFilename))) {
+	if ((stat.fd = AVI_open_output_file(const_cast<char *>(aFilename)))) {
 		stat.started = std::chrono::microseconds(Utility::bootTimeUs());
-	    stat.running = true;
 	    stat.frames  = 0;
+	    stat.fps     = NAN;
 		thread       = std::thread(&RecMjpgAvi::recordRoutine, this);
+		stat.running = true;
 		return true;
 	}
 	return false;
@@ -36,6 +51,24 @@ bool RecMjpgAvi::start(const char *aFilename)
 void RecMjpgAvi::stop()
 {
 	stat.running = false;
-	thread.join();
+	if (thread.joinable()) {
+		thread.join();
+	}
 }
 
+void RecMjpgAvi::recordRoutine()
+{
+	decltype(frame) f;
+
+	while (stat.running) {
+		f = frame;  // Safely acquired ownership through incrementing smart pointer's counter
+		if (f) {
+			AVI_write_frame(stat.fd, reinterpret_cast<char *>(const_cast<void *>(f->data())), f->size());
+			updateFps();
+		}
+	}
+
+	// Set video properties
+	calculateFps();
+	AVI_set_video(stat.fd, stat.width, stat.height, stat.fps, const_cast<char *>("MJPG"));
+}
