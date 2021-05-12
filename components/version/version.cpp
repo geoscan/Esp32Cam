1b50127361864b5f9fa05c2b41061e1cea6f3a89
Dmitry Murashov <d.murashov@geoscan.aero>
2021-05-12 12:56:46 +0300

    [version] Minor fix (buffer use), plus extended configuraton options with possibility to the baudrate of the UART used for paired device's version acquisition

--- components/version/version.cpp
+++ components/version/version.cpp
@@ -19,14 +19,16 @@ void versionInit()
 #ifdef CONFIG_VERSION_ACQUISITION_ENABLE
 
 	uint8_t buffer[CONFIG_VERSION_SERIAL_BUFFER_SIZE];
-	UartDevice uartDevice(UART_NUM_0, GPIO_NUM_3, GPIO_NUM_1, 115200, UART_PARITY_DISABLE, UART_STOP_BITS_1);
+	UartDevice uartDevice(UART_NUM_0, GPIO_NUM_3, GPIO_NUM_1, CONFIG_VERSION_UART_BAUDRATE, UART_PARITY_DISABLE, UART_STOP_BITS_1);
 
 	const auto timeEnd = std::chrono::system_clock::now() + std::chrono::microseconds(CONFIG_VERSION_STM_ACQ_TIMEOUT_US);
 
 	// Acquire and try to parse
 	while (std::chrono::system_clock::now() < timeEnd && !parser.parsed()) {
-		uartDevice.read(buffer, sizeof(buffer));
-		parser.parse(buffer, sizeof(buffer));
+		size_t nread = uartDevice.read(buffer, sizeof(buffer));
+		if (nread) {
+			parser.parse(buffer, nread);
+		}
 	}
 
 	// Apply proper formatting
