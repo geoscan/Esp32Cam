15f379728607b56355551b1d089db81723e3d788
Dmitry Murashov <d.murashov@geoscan.aero>
2021-06-30 10:37:49 +0300

    [wifi] STA mode, bug fixes | Fixed "usePassword" flag, made reconnection processing more adequate by adding the proper processing of error flags

--- components/wifi/wifi.h
+++ components/wifi/wifi.h
@@ -1,17 +1,9 @@
 #ifndef COMPONENTS_WIFI_WIFI_H
 #define COMPONENTS_WIFI_WIFI_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include "esp_err.h"
 
 void wifiStart(void);
 esp_err_t wifiConfigStaConnection(const char *targetApSsid, const char *targetApPassword, uint8_t ip[4], uint8_t gateway[4], uint8_t netmask[4]);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif // COMPONENTS_WIFI_WIFI_H
