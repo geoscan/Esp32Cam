c956198fa623d86fed1a5887e4cb5ab45a6541d9
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-02 15:24:47 +0300

    [utility: Buffer] Impl Ref | Generic buffer

--- components/utility/utility/Buffer.hpp
+++ components/utility/utility/Buffer.hpp
@@ -8,26 +8,55 @@
 #ifndef COMPONENTS_UTILITY_UTILITY_BUFFER_HPP
 #define COMPONENTS_UTILITY_UTILITY_BUFFER_HPP
 
-#include <utility>
+#include <tuple>
+#include <type_traits>
+#include <cstdint>
 
 namespace Utility {
 
-class Buffer {
-	void *mData;
-	std::size_t mSize;
-public:
-	Buffer(void *data = nullptr, std::size_t size = 0);
-	Buffer(Buffer &&);
-	Buffer &operator=(Buffer &&);
+namespace Impl {
 
-	Buffer(const Buffer &) = delete;
-	Buffer &operator=(const Buffer &) = delete;
+// How an underlying array type would be interpreted
 
-	void *data();
-	std::size_t size();
-	virtual ~Buffer() = default;
+template <typename T>
+struct InterpType {
+	using Type = T;
 };
 
+template <>
+struct InterpType<void> {
+	using Type = uint8_t;
+};
+
+template <>
+struct InterpType<const void> {
+	using Type = const uint8_t;
+};
+
+}  // namespace Impl
+
+template <typename T>
+struct Tbuffer : std::tuple<T *, std::size_t> {
+	using std::tuple<T *, std::size_t>::tuple;
+	using std::tuple<T *, std::size_t>::operator=;
+
+	using Type = T;
+
+	T *data() const;
+
+	std::size_t size() const;
+
+	typename Impl::InterpType<T>::Type &at(std::uint8_t aPos);
+
+	template <typename TypeTo>
+	Tbuffer<TypeTo> as();
+};
+
+using Buffer = typename ::Utility::Tbuffer<void>;
+using ConstBuffer = typename ::Utility::Tbuffer<const void>;
+
 }  // namespace Utility
+
+#include "Buffer.impl"
 
 #endif  // BUFFER_HPP
