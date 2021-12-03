a181792eaecef03be368e166964cb3f7015dbe60
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-03 10:45:35 +0300

    [utility: Buffer] Impl | Thold

--- components/utility/utility/Buffer.hpp
+++ components/utility/utility/Buffer.hpp
@@ -9,14 +9,16 @@
 #define COMPONENTS_UTILITY_UTILITY_BUFFER_HPP
 
 #include <tuple>
+#include <array>
 #include <type_traits>
 #include <cstdint>
+#include <initializer_list>
 
 namespace Utility {
 
 namespace Impl {
 
-// How an underlying array type would be interpreted
+// void and const void should be interpreted as std::uint8_t
 
 template <typename T>
 struct InterpType {
@@ -35,6 +37,8 @@ struct InterpType<const void> {
 
 }  // namespace Impl
 
+// Tbuffer
+
 template <typename T>
 struct Tbuffer : std::tuple<T *, std::size_t> {
 	using std::tuple<T *, std::size_t>::tuple;
@@ -50,6 +54,38 @@ struct Tbuffer : std::tuple<T *, std::size_t> {
 
 	template <typename TypeTo>
 	Tbuffer<TypeTo> as();
+};
+
+template <typename Tto, typename Tfrom>
+Tbuffer<Tto> toBuffer(Tfrom *aFromPtr, std::size_t aBufferSize);
+
+///
+/// \brief toBuffer Constructs buffer from an instance for which
+/// ::data and ::size methods are defined
+///
+/// \tparam TdataTraitFrom - some instance defining ::data and ::size
+/// parameters
+///
+/// \return
+///
+template <typename Tto, typename TdataTraitFrom>
+Tbuffer<Tto> toBuffer(TdataTraitFrom &&);
+
+// Thold
+
+template <typename T, std::size_t N>
+struct Thold : std::array<T, N> {
+	using std::array<T, N>::data;
+	using std::array<T, N>::operator=;
+	using std::array<T, N>::array;
+
+	void setSize(std::size_t);
+	std::size_t size() const;
+
+	Tbuffer<T> asBuffer();
+
+private:
+	std::size_t mSize = 0;
 };
 
 using Buffer = typename ::Utility::Tbuffer<void>;
