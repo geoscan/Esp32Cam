665eaed91ab2891080660bd667301639da25420f
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-23 17:06:32 +0300

    [mav:Dispatcher] Impl

---
+++ components/mav/Dispatcher.cpp
@@ -0,0 +1,60 @@
+//
+// Dispatcher.cpp
+//
+// Created on: Dec 21, 2021
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#include "utility/Subscription.hpp"
+#include "Mavlink.hpp"
+#include "Marshalling.hpp"
+#include "Unmarshalling.hpp"
+#include "Microservice/GsNetwork.hpp"
+#include "Dispatcher.hpp"
+
+Mav::Dispatcher::Dispatcher():
+	key{{&Dispatcher::onMavlinkUartReceived, this}, {&Dispatcher::onMavlinkUdpReceived, this}},
+	micAggregate{}
+{
+}
+
+Mav::Microservice::Ret Mav::Dispatcher::process(Utility::Subscription::Message &aMessage)
+{
+	auto ret = Microservice::Ret::Ignored;
+	unmarshalling.push(aMessage.payload);  // parse incoming message, check whether it is a mavlink
+
+	if (unmarshalling.size()) {
+		auto message = unmarshalling.back();
+		ret = micAggregate.process(message);
+
+		if (ret == Microservice::Ret::Response) {
+			marshalling.push(message);
+			unmarshalling.pop();
+		}
+	}
+
+	return ret;
+}
+
+Utility::Subscription::RoutingResult Mav::Dispatcher::onMavlinkUartReceived(Utility::Subscription::Message &aMessage)
+{
+	switch (process(aMessage)) {
+		case Microservice::Ret::Ignored:  // forward the message to UDP interface
+			Utility::Subscription::Key::MavlinkUdpSend::notify(aMessage);
+			break;
+
+		case Microservice::Ret::Response:  // send response back
+			aMessage.payload = marshalling.back().asBuffer();
+			Utility::Subscription::Key::MavlinkUartSend::notify(aMessage);
+			marshalling.pop();
+			break;
+
+		default:  // Message has been processed from inside, no actions required
+			break;
+	};
+}
+
+Utility::Subscription::RoutingResult Mav::Dispatcher::onMavlinkUdpReceived(Utility::Subscription::Message &aMessage)
+{
+	Utility::Subscription::Key::MavlinkUartSend::notify(aMessage);  // forward the message to UART interface
+}
---
+++ components/mav/priv_include/Dispatcher.hpp
@@ -0,0 +1,56 @@
+//
+// Dispatcher.hpp
+//
+// Created on: Dec 21, 2021
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+// Accepts messages passed through a callback mechanism, and passes those to
+// encapsulated microservices.
+
+#ifndef MAV_PRIV_INCLUDE_DISPATCHER_HPP
+#define MAV_PRIV_INCLUDE_DISPATCHER_HPP
+
+#include "utility/Subscription.hpp"
+#include "Microservice.hpp"
+#include "Microservice/Aggregate.hpp"
+#include <list>
+#include <memory>
+
+struct __mavlink_message;
+using mavlink_message_t = __mavlink_message;
+
+namespace Mav {
+
+class Marshalling;
+class Unmarshalling;
+
+namespace Mic {
+class GsNetwork;
+}
+
+class Dispatcher {
+public:
+	Dispatcher();
+
+protected:
+	Mav::Microservice::Ret process(Utility::Subscription::Message &aMessage);
+
+private:
+	Utility::Subscription::RoutingResult onMavlinkUartReceived(Utility::Subscription::Message &aMessage);
+	Utility::Subscription::RoutingResult onMavlinkUdpReceived(Utility::Subscription::Message &aMessage);
+
+private:
+	struct {
+		Utility::Subscription::Key::MavlinkUartReceived fromUart;
+		Utility::Subscription::Key::MavlinkUdpReceived fromUdp;
+	} key;
+
+	Marshalling marshalling;
+	Unmarshalling unmarshalling;
+	Mav::Mic::Aggregate<Mic::GsNetwork> micAggregate;
+};
+
+}  // namespace Mav
+
+#endif // MAV_PRIV_INCLUDE_DISPATCHER_HPP
