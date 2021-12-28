18129663eeb15556f7b6cfef3770298d6f494b27
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-28 14:56:05 +0300

    [mav:mav] Impl | init function

---
+++ components/mav/include/mav/mav.hpp
@@ -0,0 +1,17 @@
+//
+// mav.hpp
+//
+// Created on: Dec 28, 2021
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#ifndef MAV_INCLUDE_MAV_MAV_HPP
+#define MAV_INCLUDE_MAV_MAV_HPP
+
+namespace Mav {
+
+void init();
+
+}  // namespace Mav
+
+#endif  // MAV_INCLUDE_MAV_MAV_HPP
