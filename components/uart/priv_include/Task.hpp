ffbfdba893508bec4667685e11c5b301e1cd348f
DmitryMurashov <dmtr.murashov@gmail.com>
2021-12-28 13:22:26 +0300

    [uart:Task] Impl
    UART task pool

---
+++ components/uart/priv_include/Task.hpp
@@ -0,0 +1,36 @@
+//
+// UartTask.hpp
+//
+// Created on: Dec 28, 2021
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#ifndef UART_UARTTASK_HPP
+#define UART_UARTTASK_HPP
+
+#include <list>
+#include <array>
+#include "utility/Subscription.hpp"
+
+class UartDevice;
+
+namespace Uart {
+
+class Task {
+public:
+	template <class ...Tuarts>
+	Task(Tuarts &&...aArgs);
+
+	void operator()();
+
+private:
+	static constexpr unsigned kBufferSize = 512;
+	std::array<std::uint8_t, kBufferSize> buffer;
+	std::list<UartDevice> uartDevices;
+};
+
+}  // namespace Uart
+
+#include "Task.impl"
+
+#endif  // UART_UARTTASK_HPP
