27018474974bb325fef38ad635895f9fdd4fd8e2
DmitryMurashov <dmtr.murashov@gmail.com>
2022-01-11 16:57:38 +0300

    [uart:uart] Impl | OnSend static instance
    Added the UART requests handler

--- components/uart/uart.cpp
+++ components/uart/uart.cpp
@@ -9,6 +9,7 @@
 #include "UartDevice.hpp"
 #include "Task.hpp"
 #include "utility/Threading.hpp"
+#include "OnSend.hpp"
 
 #include <thread>
 
@@ -21,7 +22,9 @@ void init()
 	static UartDevice devices[] = {
 		UartDevice{UART_NUM_0, GPIO_NUM_3, GPIO_NUM_1, CONFIG_WIFI_UART_BRIDGE_BAUDRATE, UART_PARITY_DISABLE, UART_STOP_BITS_1}  // MAVLink / Autopilot link
 	};
-	static Task sTask {&devices[0]};
+	static Task sTask{devices[0]};
+	static OnSend<3> onSend{devices[0]};
+	(void)onSend;
 	task = &sTask;
 }
 
