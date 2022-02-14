b5d0a6689ef2a70708cd3f232918dd49c78c0422
DmitryMurashov <dmtr.murashov@gmail.com>
2022-02-14 16:23:35 +0300

    [socket:socket] Impl | Default UDP port

--- components/socket/socket.cpp
+++ components/socket/socket.cpp
@@ -13,12 +13,26 @@
 
 using namespace Sock;
 
+static void apiInit(Sock::Api &aApi)
+{
+	constexpr auto kUdpOpen = {8001 /* AP protocol (MAVLink) */};
+
+	asio::error_code err;
+
+	for (auto port : kUdpOpen) {
+		aApi.openUdp(port, err);
+	}
+}
+
 void init(asio::io_context &aIoContext)
 {
 	static constexpr const std::chrono::milliseconds taskPollPeriod{200};
 	static std::mutex syncAsyncMutex;
 	static Task task{aIoContext, taskPollPeriod, syncAsyncMutex};
 	static Sock::Api api{aIoContext, syncAsyncMutex};
+
+	apiInit(api);
+
 	static std::thread thread{&Task::run, &task};
 	(void)api;
 	(void)thread;
