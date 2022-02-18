9a7facac52bdad7b2e62849862b4e18adbb196ec
DmitryMurashov <dmtr.murashov@gmail.com>
2022-02-18 16:03:25 +0300

    [mav:Dispatcher] Opt | Exact size of sent message

--- components/mav/Dispatcher.cpp
+++ components/mav/Dispatcher.cpp
@@ -28,7 +28,7 @@ Mav::Microservice::Ret Mav::Dispatcher::process(Utility::ConstBuffer aBuffer)
 		ret = micAggregate.process(message);
 
 		if (ret == Microservice::Ret::Response) {
-			marshalling.push(message);
+			marshallingSize = marshalling.push(message);
 		}
 		unmarshalling.pop();
 	}
@@ -59,7 +59,7 @@ Sub::Rout::OnMavlinkReceived::Ret Mav::Dispatcher::onMavlinkReceived(Sub::Rout::
 
 		case Microservice::Ret::Response:  // send response back
 			response.setType(Sub::Rout::Response::Type::Response);
-			response.payload = Sub::Rout::Payload{&marshalling.back(), sizeof(marshalling.back())};
+			response.payload = Sub::Rout::Payload{&marshalling.back(), marshallingSize};
 
 			break;
 
