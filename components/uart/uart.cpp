1df993bdf6bcc73ad6c19ffca2e06447fa4b4993
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-21 14:56:52 +0300

    [uart:uart] Bug | Insufficient stack size

--- components/uart/uart.cpp
+++ components/uart/uart.cpp
@@ -32,7 +32,7 @@ void init()
 void start()
 {
 	init();
-	Utility::Threading::Config(true).core(0).stack(CONFIG_ESP32_PTHREAD_TASK_STACK_SIZE_DEFAULT + 1024);
+	Utility::Threading::Config(true).core(0).stack(CONFIG_ESP32_PTHREAD_TASK_STACK_SIZE_DEFAULT + 2048);
 	static std::thread thread{&Task::operator(), task};
 }
 
