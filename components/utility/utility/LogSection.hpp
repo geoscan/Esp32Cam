508deba330685ca01fcaf05a76adca9ecd8d0ab6
DmitryMurashov <dmtr.murashov@gmail.com>
2022-03-28 14:44:53 +0300

    [utility:LogSection] Ref | Comments

--- components/utility/utility/LogSection.hpp
+++ components/utility/utility/LogSection.hpp
@@ -5,9 +5,12 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
+// Enables section-wide debug output.
+
 #ifndef UTILITY_UTILITY_LOGSECTION_HPP_
 #define UTILITY_UTILITY_LOGSECTION_HPP_
 
+// RAII wrapper over debug output
 #define GS_UTILITY_LOG_SECTION_SUFF_LEVEL(tag, context, cl, instance, command) \
 struct cl {\
 	inline cl() {command(tag, context " enter"); } \
@@ -15,10 +18,14 @@ struct cl {\
 } instance; \
 (void) instance
 
+// Implementation detail
 #define GS_UTILITY_LOG_DEF_APPEND(x, y) x ## y
 #define GS_UTILITY_LOG_SECTION_IMPL(tag, context, suff, command) GS_UTILITY_LOG_SECTION_SUFF_LEVEL(tag, context, GS_UTILITY_LOG_DEF_APPEND(Log,suff), GS_UTILITY_LOG_DEF_APPEND(log,suff), command)
 
+// Debug section - "verbose" level
 #define GS_UTILITY_LOG_SECTIONV(tag, context) GS_UTILITY_LOG_SECTION_IMPL(tag,context,__LINE__, ESP_LOGV)
+
+// DEbug section - "debug" level
 #define GS_UTILITY_LOG_SECTIOND(tag, context) GS_UTILITY_LOG_SECTION_IMPL(tag,context,__LINE__, ESP_LOGD)
 
 #endif // UTILITY_UTILITY_LOGSECTION_HPP_
