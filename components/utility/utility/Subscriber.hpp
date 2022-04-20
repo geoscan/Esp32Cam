0a8fe8003e91ad30ed8fb60bb8129af24f375a31
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-20 13:41:57 +0300

    [utility:Subscriber] Impl

---
+++ components/utility/utility/Subscriber.hpp
@@ -0,0 +1,48 @@
+//
+// Subscriber.hpp
+//
+// Created on: Apr 20, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+// Eliminates subscriber / sender boilerplate
+//
+
+#ifndef UTILITY_UTILITY_SUBSCRIBER_HPP_
+#define UTILITY_UTILITY_SUBSCRIBER_HPP_
+
+namespace Utility {
+namespace Sub {
+
+///
+/// \tparam Tsub  Subscriber class
+/// \tparam Targs Parameters a subscriber accepts
+/// \pre    Tsub  Must implement `onSubscription(Targs...)`
+///
+template <class Tsub, class ...Targs>
+class Sender1to1 {
+public:
+	void addSubscriber(Tsub &aSub) {
+		subscriber = &aSub;
+	}
+
+	void removeSubscriber()
+	{
+		subscriber = 0;
+	}
+
+protected:
+	void send(Targs ...aArgs)
+	{
+		if (subscriber) {
+			subscriber->onSubscription(aArgs...);
+		}
+	}
+
+private:
+	Tsub *subscriber;
+};
+
+}  // namespace Sub
+}  // namespace Utility
+
+#endif // UTILITY_UTILITY_SUBSCRIBER_HPP_
