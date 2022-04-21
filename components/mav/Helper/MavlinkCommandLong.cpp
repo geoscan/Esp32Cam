d3834565c8f9e77da025ffa321b5ff76d485b32a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-21 13:53:06 +0300

    [mav:MavlinkCommandLong] Bug | decltype(float)

--- components/mav/Helper/MavlinkCommandLong.cpp
+++ components/mav/Helper/MavlinkCommandLong.cpp
@@ -20,8 +20,8 @@ mavlink_command_long_t MavlinkCommandLong::makeFrom(const mavlink_command_int_t 
 	res.param2 = aIn.param2;
 	res.param3 = aIn.param3;
 	res.param4 = aIn.param4;
-	res.param5 = static_cast<decltype(float)>(aIn.x);
-	res.param6 = static_cast<decltype(float)>(aIn.y);
+	res.param5 = static_cast<float>(aIn.x);
+	res.param6 = static_cast<float>(aIn.y);
 	res.param7 = aIn.z;
 	res.command = aIn.command;
 
