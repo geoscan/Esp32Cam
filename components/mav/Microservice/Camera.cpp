64b0b725b17b48f08ed51beb2a18df4d6704ec2e
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-21 13:54:11 +0300

    [mav:Camera] Impl | Unpacking MAVLink Command Protocol messages

--- components/mav/Microservice/Camera.cpp
+++ components/mav/Microservice/Camera.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "Microservice/Camera.hpp"
+#include "Helper/MavlinkCommandLong.hpp"
 #include "sub/Rout.hpp"
 #include "Globals.hpp"
 
@@ -28,10 +29,39 @@ void Camera::onHrTimer()
 
 Microservice::Ret Camera::process(mavlink_message_t &aMessage, OnResponseSignature aOnResponse)
 {
-	(void)aMessage;
-	(void)aOnResponse;
+	Microservice::Ret ret = Microservice::Ret::Ignored;
 
-	return {Microservice::Ret::Ignored};
+	switch (aMessage.msgid) {
+		case MAVLINK_MSG_ID_COMMAND_LONG: {
+			mavlink_command_long_t mavlinkCommandLong;
+			(void)mavlinkCommandLong;
+			mavlink_msg_command_long_decode(&aMessage, &mavlinkCommandLong);
+			ret = processCommand(mavlinkCommandLong, aMessage, aOnResponse);
+
+			break;
+		}
+
+		case MAVLINK_MSG_ID_COMMAND_INT: {
+			mavlink_command_int_t mavlinkCommandInt;
+			(void)mavlinkCommandInt;
+			mavlink_msg_command_int_decode(&aMessage, &mavlinkCommandInt);
+			mavlink_command_long_t mavlinkCommandLong = Hlpr::MavlinkCommandLong::makeFrom(mavlinkCommandInt);
+			ret = processCommand(mavlinkCommandLong, aMessage, aOnResponse);
+
+			break;
+		}
+
+		default:
+
+			break;
+	}
+
+	return ret;
+}
+
+Microservice::Ret Camera::processCommand(mavlink_command_long_t &aMavlinkCommandLong, mavlink_message_t &aMessage, Microservice::OnResponseSignature aOnResponse)
+{
+	return {};
 }
 
 }  // namespace Mic
