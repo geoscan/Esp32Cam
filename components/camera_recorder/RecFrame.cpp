d158bf391d1681cab220e8e3d0d3b273d48a64b7
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-25 16:10:28 +0300

    Revert "[camera_recorder:RecFrame] Int | Module API: Make a shot"
    This reverts commit d502e850ccc4f50cf6cca08ee6e65a306404dfa2.

--- components/camera_recorder/RecFrame.cpp
+++ components/camera_recorder/RecFrame.cpp
@@ -23,10 +23,6 @@ void RecFrame::onNewFrame(Key::Type frame)
 	sync.sem.release();  // Notify that frame's been written
 }
 
-RecFrame::RecFrame() : keys{{&RecFrame::start, this}}
-{
-}
-
 bool RecFrame::start(const char *aFilename)
 {
 	{
