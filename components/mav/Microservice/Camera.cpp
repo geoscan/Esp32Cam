889048e64db529f544f473053e5505a0cd251de3
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-25 16:30:19 +0300

    Revert "[mav:Camera] Enh | ::start*Capture(): more applicable return type"
    This reverts commit 092927eee989a55425a0eff073ceb0f7ac8b7ca6.

--- components/mav/Microservice/Camera.cpp
+++ components/mav/Microservice/Camera.cpp
@@ -208,7 +208,7 @@ Microservice::Ret Camera::processCmdImageStartCapture(mavlink_command_long_t &aM
 	mavlink_message_t &aMessage, Microservice::OnResponseSignature aOnResponse)
 {
 	sdFatInit();
-	MAV_RESULT mavResult = MAV_RESULT_UNSUPPORTED;
+	MAV_RESULT mavResult = MAV_RESULT_FAILED;
 	static constexpr std::size_t kNameMaxLen = 6;
 	char filename[kNameMaxLen] = {0};
 
