54ed2d770cf7173d2a9c5f6f17585ef4b7442642
DmitryMurashov <dmtr.murashov@gmail.com>
2022-04-25 15:29:36 +0300

    [mav:Camera] Enh | Logging

--- components/mav/Microservice/Camera.cpp
+++ components/mav/Microservice/Camera.cpp
@@ -226,8 +226,8 @@ Microservice::Ret Camera::processCmdImageStartCapture(mavlink_command_long_t &aM
 		}
 	}
 
-	ESP_LOGI(Mav::kDebugTag, "Camera::processCmdImageStartCapture, request to make a shot, response code: %d",
-		mavResult);
+	ESP_LOGI(Mav::kDebugTag, "Camera::processCmdImageStartCapture, request to make a shot, frame name \"%s\""
+		"response code \"%d\"", filename, mavResult);
 
 	// Send ACK w/ value depending on whether or not the capture was successful
 	{
