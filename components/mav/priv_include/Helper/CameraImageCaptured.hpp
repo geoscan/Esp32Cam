75384df78e60225fadd8f5db1f4dcda7b9fe305b
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-19 09:57:22 +0300

    [mav:CameraImageCaptured] Int | Trait for CAMERA_IMAGE_CAPTURED encoder

--- components/mav/priv_include/Helper/CameraImageCaptured.hpp
+++ components/mav/priv_include/Helper/CameraImageCaptured.hpp
@@ -9,11 +9,12 @@
 #define MAV_PRIV_INCLUDE_HELPER_CAMERAIMAGECAPTURED_HPP_
 
 #include "Mavlink.hpp"
+#include "Common.hpp"
 
 namespace Mav {
 namespace Hlpr {
 
-struct CameraImageCaptured : mavlink_camera_image_captured_t {
+struct CameraImageCaptured : mavlink_camera_image_captured_t, Cmn::Impl::Pack<mavlink_camera_image_captured_t> {
 	static CameraImageCaptured make(decltype(mavlink_camera_image_captured_t::image_index) aImageIndex,
 		decltype(mavlink_camera_image_captured_t::capture_result) aCaptureResult, const char *fileName);
 	static CameraImageCaptured make(decltype(mavlink_camera_image_captured_t::image_index) aImageIndex,
