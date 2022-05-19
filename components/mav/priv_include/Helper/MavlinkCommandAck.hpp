b2cf09e09af6e9f75e3dccd01533967b474a6086
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-19 09:47:04 +0300

    [mav:MavlinkCommandAck] Int | `Pack` mix-in

--- components/mav/priv_include/Helper/MavlinkCommandAck.hpp
+++ components/mav/priv_include/Helper/MavlinkCommandAck.hpp
@@ -9,11 +9,12 @@
 #define MAV_PRIV_INCLUDE_HELPER_MAVLINKCOMMANDACK_HPP_
 
 #include "Mavlink.hpp"
+#include "Common.hpp"
 
 namespace Mav {
 namespace Hlpr {
 
-struct MavlinkCommandAck : public mavlink_command_ack_t {
+struct MavlinkCommandAck : Cmn::Impl::Pack<mavlink_command_ack_t> {
 	static MavlinkCommandAck makeFrom(const mavlink_message_t &aMessage,
 		decltype(mavlink_command_ack_t::command) aCommand,
 		decltype(mavlink_command_ack_t::result) aResult = MAV_RESULT_ACCEPTED);
