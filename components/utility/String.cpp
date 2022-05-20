d6dfda90ede6d0165c5f0fb158e3d5ed0b853cec
DmitryMurashov <dmtr.murashov@gmail.com>
2022-05-20 18:06:12 +0300

    [utility:String] Bug | checkEndswith

--- components/utility/String.cpp
+++ components/utility/String.cpp
@@ -15,13 +15,13 @@ namespace Str {
 bool checkEndswith(const char *aStr, const char *aSuffix)
 {
 	const auto aStrLen = std::strlen(aStr);
-	const auto aSuffixLen = std::strlen(aStr);
+	const auto aSuffixLen = std::strlen(aSuffix);
 	bool ret = false;
 
 	if (aStrLen > 0 && aSuffixLen > 0 && aStrLen >= aSuffixLen) {
 		auto *pos = std::find_end(aStr, aStr + aStrLen, aSuffix, aSuffix + aSuffixLen);
 
-		if (aStr + aStrLen != pos) {
+		if (aStr + aStrLen - aSuffixLen == pos) {
 			ret = true;
 		}
 	}
