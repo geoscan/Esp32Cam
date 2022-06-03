c08da11383d9943d24d089493f49166adc4567ed
DmitryMurashov <dmtr.murashov@gmail.com>
2022-06-03 15:09:19 +0300

    [wq:wq] Impl

---
+++ components/wq/include/wq/wq.hpp
@@ -0,0 +1,17 @@
+//
+// wq.hpp
+//
+// Created on: Jun 03, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(WQ_INCLUDE_WQ_HPP_)
+#define WQ_INCLUDE_WQ_HPP_
+
+namespace Wq {
+
+void start();
+
+}  // namespace Wq
+
+#endif // WQ_INCLUDE_WQ_HPP_
