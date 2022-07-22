a141fd6eb55cce65127cda8e2d8e793b997cd6f0
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-22 10:41:37 +0300

    [utility:InstanceRegistry] Impl | `SyncedStorage`

--- components/utility/utility/comm/InstanceRegistry.hpp
+++ components/utility/utility/comm/InstanceRegistry.hpp
@@ -9,6 +9,7 @@
 #define UTILITY_UTILITY_COMM_INSTANCEREGISTRY_HPP_
 
 #include <algorithm>
+#include <mutex>
 
 namespace Utility {
 namespace Comm {
@@ -30,6 +31,35 @@ struct InstanceRegistry {
 
 		if (std::end(instances) != it) {
 			instances.erase(it);
+		}
+	}
+};
+
+template <class T, template <class ...> class Tc>
+struct SyncedStorage {
+private:
+	std::mutex mutex;
+	Tc<T> storage;
+public:
+	template <class ...Ts>
+	SyncedStorage(Ts &&...aArgs) : mutex{}, storage{std::forward<Ts>(aArgs)...}
+	{
+	}
+
+	template <class ...Ts>
+	T &emplace(Ts &&...aArgs)
+	{
+		std::lock_guard<std::mutex> lock{mutex};
+		(void)lock;
+
+		T t{std::forward<Ts>(aArgs)...};
+
+		auto it = std::find(std::begin(storage), std::end(storage), t);
+
+		if (std::end(storage) != it) {
+			return (*std::inserter(storage, it) = std::move(t));
+		} else {
+			return *it;
 		}
 	}
 };
