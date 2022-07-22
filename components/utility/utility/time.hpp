5cc433a69f2def9670915719be70168e7c343412
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-22 14:41:59 +0300

    [utility:time] Impl | Chrono-based `wait`

--- components/utility/utility/time.hpp
+++ components/utility/utility/time.hpp
@@ -2,9 +2,9 @@
 #define COMPONENTS_UTILITY_TIME_HPP
 
 #include <esp_timer.h>
+#include <chrono>
 
 namespace Utility {
-
 using Time = int64_t;
 
 // Wait for a certain duration
@@ -16,6 +16,15 @@ Time bootTimeUs();
 // Check if required time period has already passed
 bool expired(const Time sinceUs, Time periodUs);
 
+namespace Tim {
+
+template <class Trep, class Tper>
+void taskDelay(const std::chrono::duration<Trep, Tper> &aDuration)
+{
+	waitMs(std::chrono::duration_cast<std::chrono::milliseconds>(aDuration).count());
+}
+
+}  // namespace Tim
 }  // namespace Utility
 
 #endif  // COMPONENTS_UTILITY_TIME_HPP
