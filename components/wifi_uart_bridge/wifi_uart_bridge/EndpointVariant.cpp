76cc615e2db3b0de27a3fda45b9217b01f681efd
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-28 15:50:41 +0300

    [wifi_uart_bridge:EndpointVariant] Impl | Additional operators

--- components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.cpp
+++ components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.cpp
@@ -19,4 +19,14 @@ bool operator==(const UdpEndpoint &aLhs, const UdpPort &aRhs)
 	return std::get<1>(aLhs) == std::get<0>(aRhs);
 }
 
+bool operator<(const TcpEndpoint &aLhs, const TcpPort &aRhs)
+{
+	return std::get<1>(aLhs) < std::get<0>(aRhs);
+}
+
+bool operator<(const UdpEndpoint &aLhs, const UdpPort &aRhs)
+{
+	return std::get<1>(aLhs) < std::get<0>(aRhs);
+}
+
 }  // namespace Bdg
--- components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.hpp
+++ components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.hpp
@@ -17,15 +17,20 @@ namespace Bdg {
 /// \brief Some endpoints do not need additional identification (such as uart num or port).
 ///
 enum class NamedEndpoint {
-	None,
+	Mavlink,  ///< Mavlink protocol implementation
+	MavlinkIpPack,  ///< Pack and forward data received over IP
+	UartMavlinkForwarded,  ///< Mavlink packets forwarded from a dedicated UART
+	UdpMavlinkForwarded,  ///< Mavlink packets forwarded from a dedicated UDP port
 };
 
 using TcpEndpoint = typename std::tuple<asio::ip::tcp::endpoint, std::uint16_t /* Port */>;
 using UdpEndpoint = typename std::tuple<asio::ip::udp::endpoint, std::uint16_t /* Port */>;
 using UartEndpoint = typename std::tuple<std::uint8_t /* UART num */>;
 struct TcpPort : std::tuple<std::uint16_t> {  // Inheritance from `std::tuple` is used to differentiate b/w types while retaining comparison operators implemented for `std::tuple`.
+	using std::tuple<std::uint16_t>::tuple;
 };
 struct UdpPort : std::tuple<std::uint16_t> {
+	using std::tuple<std::uint16_t>::tuple;
 };
 
 using EndpointVariant = mapbox::util::variant<NamedEndpoint, TcpEndpoint, UdpEndpoint, UartEndpoint>;
@@ -33,6 +38,8 @@ using EndpointVariant = mapbox::util::variant<NamedEndpoint, TcpEndpoint, UdpEnd
 /// \brief Enalbes using rules that apply on mere equality of originators' ports, ignoring addresses of the actual
 /// remote senders
 ///
+bool operator<(const TcpEndpoint &aLhs, const TcpPort &aRhs);
+bool operator<(const UdpEndpoint &aLhs, const UdpPort &aRhs);
 bool operator==(const TcpEndpoint &aLhs, const TcpPort &aRhs);
 bool operator==(const UdpEndpoint &aLhs, const UdpPort &aRhs);
 
