27b58fd80a3dba8918af3f8912b080a5640fbaa9
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-28 17:08:57 +0300

    [wifi_uart_bridge:EndpointVariant] Enh | `asAlternative` instead of custom comparison operators

--- components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.cpp
+++ components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.cpp
@@ -9,24 +9,30 @@
 
 namespace Bdg {
 
-bool operator==(const TcpEndpoint &aLhs, const TcpPort &aRhs)
+/// \brief Enables additional interpretation of an identity
+///
+/// \details In some cases, we only need to compare rules by target ports or remote senders' identities. E.g. (Sender
+/// endpoint, Port) pairs may also be interpreted as (Sender endpoint) or (Port). `asAlternative` generalizes this
+/// feature.
+///
+std::vector<EndpointVariant> EndpointVariant::asAlternative()
 {
-	return std::get<1>(aLhs) == std::get<0>(aRhs);
-}
+	constexpr std::size_t kSizeHint = 2;
+	std::vector<EndpointVariant> ret{kSizeHint};
+	match(
+		[&ret](const UdpEndpoint &aUdpEndpoint)
+		{
+			ret.emplace_back(std::get<0>(aUdpEndpoint));
+			ret.emplace_back(UdpPort{std::get<1>(aUdpEndpoint)});
+		},
+		[&ret](const TcpEndpoint &aTcpEndpoint)
+		{
+			ret.emplace_back(std::get<0>(aTcpEndpoint));
+			ret.emplace_back(TcpPort{std::get<1>(aTcpEndpoint)});
+		},
+		[](...) {});
 
-bool operator==(const UdpEndpoint &aLhs, const UdpPort &aRhs)
-{
-	return std::get<1>(aLhs) == std::get<0>(aRhs);
-}
-
-bool operator<(const TcpEndpoint &aLhs, const TcpPort &aRhs)
-{
-	return std::get<1>(aLhs) < std::get<0>(aRhs);
-}
-
-bool operator<(const UdpEndpoint &aLhs, const UdpPort &aRhs)
-{
-	return std::get<1>(aLhs) < std::get<0>(aRhs);
+	return ret;
 }
 
 }  // namespace Bdg
--- components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.hpp
+++ components/wifi_uart_bridge/wifi_uart_bridge/EndpointVariant.hpp
@@ -8,6 +8,7 @@
 #include <mapbox/variant.hpp>
 #include <asio.hpp>
 #include <tuple>
+#include <vector>
 
 #if !defined(WIFI_UART_BRIDGE_WIFI_UART_BRIDGE_ENDPOINTVARIANT_HPP_)
 #define WIFI_UART_BRIDGE_WIFI_UART_BRIDGE_ENDPOINTVARIANT_HPP_
@@ -21,6 +22,7 @@ enum class NamedEndpoint {
 	MavlinkIpPack,  ///< Pack and forward data received over IP
 	UartMavlinkForwarded,  ///< Mavlink packets forwarded from a dedicated UART
 	UdpMavlinkForwarded,  ///< Mavlink packets forwarded from a dedicated UDP port
+	None,
 };
 
 using TcpEndpoint = typename std::tuple<asio::ip::tcp::endpoint, std::uint16_t /* Port */>;
@@ -33,15 +35,14 @@ struct UdpPort : std::tuple<std::uint16_t> {
 	using std::tuple<std::uint16_t>::tuple;
 };
 
-using EndpointVariant = mapbox::util::variant<NamedEndpoint, TcpEndpoint, UdpEndpoint, UartEndpoint>;
+using EndpointVariantBase = mapbox::util::variant<NamedEndpoint, TcpEndpoint, UdpEndpoint, UartEndpoint,
+	asio::ip::udp::endpoint, asio::ip::tcp::endpoint>;
 
-/// \brief Enalbes using rules that apply on mere equality of originators' ports, ignoring addresses of the actual
-/// remote senders
-///
-bool operator<(const TcpEndpoint &aLhs, const TcpPort &aRhs);
-bool operator<(const UdpEndpoint &aLhs, const UdpPort &aRhs);
-bool operator==(const TcpEndpoint &aLhs, const TcpPort &aRhs);
-bool operator==(const UdpEndpoint &aLhs, const UdpPort &aRhs);
+struct EndpointVariant : EndpointVariantBase
+{
+	using EndpointVariantBase::EndpointVariantBase;
+	std::vector<EndpointVariant> asAlternative();
+};
 
 }  // namespace Bdg
 
