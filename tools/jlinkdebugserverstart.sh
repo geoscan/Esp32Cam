93564968dabe308ec791654a6d2b8173ac805a46
DmitryMurashov <dmtr.murashov@gmail.com>
2022-07-29 12:53:51 +0300

    [tools:jlinkdebugserverstart] Bug

--- tools/jlinkdebugserverstart.sh
+++ tools/jlinkdebugserverstart.sh
@@ -2,7 +2,6 @@
 
 HERE=$(dirname "${BASH_SOURCE[0]}")
 HERE=$(realpath $HERE)
-cd $HERE/.. && make jlinkespconnect
 PYTHON=/usr/bin/python3
 
-$PYTHON $here/jlink_openocd_scripts/jlinkgdbrun.py &
+$PYTHON $HERE/jlink_openocd_scripts/jlinkgdbrun.py
