beb7130aa9cd42b6fa0c3a725a18eb595073174a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-02 18:38:25 +0300

    [utility:LogSection] Impl | Method-wise logging

--- components/utility/utility/LogSection.hpp
+++ components/utility/utility/LogSection.hpp
@@ -28,6 +28,54 @@ struct cl {\
 // Debug section - "debug" level
 #define GS_UTILITY_LOG_SECTIOND(tag, context) GS_UTILITY_LOG_SECTION_IMPL(tag,context,__LINE__, ESP_LOGD)
 
-#define GS_UTILITY_LOGV_METHOD(tag, cls, method, ...) ESP_LOGV(tag, #cls "::" #method "() " __VA_ARGS__)
+template <class R, class T, class ...Ts>
+struct GsUtilityLogMember {
+	template <R (T::* M)(Ts...)>
+	struct Impl {
+	};
+};
+
+template <class R, class T, class ...Ts>
+struct GsUtilityLogFunction {
+	template <R (*F)(Ts...)>
+	struct Impl {
+	};
+};
+
+template <class T, class R, class ...Ts>
+constexpr auto gsUtilityRevealType(R (T::*)(Ts...)) -> GsUtilityLogMember<R, T, Ts...>
+{
+	return {};
+}
+
+template <class T, class R, class ...Ts>
+constexpr auto gsUtilityRevealType(R (*)(Ts...)) -> GsUtilityLogFunction<R, T, Ts...>
+{
+	return {};
+}
+
+#define GS_UTILITY_LOG_METHOD_MARKER_TYPE(cls, method) \
+	typename decltype(gsUtilityRevealType< cls >(&cls::method))::Impl<&cls::method>
+
+template <class T>
+struct GsUtilityLogvMethod {
+	static constexpr bool enabled = false;
+};
+
+#define GS_UTILITY_LOGV_METHOD_SET_ENABLED(cls, method, en) \
+template<> \
+struct GsUtilityLogvMethod< GS_UTILITY_LOG_METHOD_MARKER_TYPE(cls, method) > { \
+	static constexpr bool enabled = en; \
+};
+
+#define GS_UTILITY_LOGV_METHOD(tag, cls, method, ...) \
+struct cls##method ; \
+do { \
+	if (GsUtilityLogvMethod< GS_UTILITY_LOG_METHOD_MARKER_TYPE(cls, method) >::enabled) { \
+		ESP_LOGV(tag, #cls "::" #method "() " __VA_ARGS__ ); \
+	} \
+} while (0)
+
+//#define GS_UTILITY_LOGV_METHOD(tag, cls, method, ...) ESP_LOGV(tag, #cls "::" #method "() " __VA_ARGS__)
 
 #endif // UTILITY_UTILITY_LOGSECTION_HPP_
