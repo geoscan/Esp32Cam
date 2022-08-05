9f370f7e6f208c60ebf4e749d003a7b61b07e82a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-05 12:22:49 +0300

    Revert "README Revert "[http:CMakeLists] Build | Required modules: `utility`""
    This reverts commit cc258e2bbb91652aaaf0e1b4b6aca542bdd60b4f.

--- components/http/pages/version.cpp
+++ components/http/pages/version.cpp
@@ -1,23 +1,41 @@
+#include "pages.h"
+#include "Ov2640.hpp"
+#include "version.hpp"
+#include "utility/mod/ModuleBase.hpp"
+#include <cstring>
 #include <esp_err.h>
 #include <esp_http_server.h>
 #include <cJSON.h>
 
-#include "pages.h"
-#include "Ov2640.hpp"
-#include "version.hpp"
-
 esp_err_t infoPageHandler(httpd_req_t *req)
 {
 	auto *versions = cJSON_CreateArray();
-	cJSON_AddItemToArray(versions, cJSON_CreateString(ESP32_FIRMWARE_VERSION));
+	cJSON_AddItemToArray(versions, cJSON_CreateString(ESP32_FIRMWARE_VERSION));  // Add ESP version to the JSON object
 
-	std::string connectedSerialVersion = connectedSerialVersionGet();
-	if (connectedSerialVersion.size()) {
-		cJSON_AddItemToArray(versions, cJSON_CreateString(connectedSerialVersion.c_str()));
+	// Form the autopilot's version string
+	{
+		std::uint32_t revMajor = 0;
+		std::uint32_t revMinor = 0;
+		std::uint32_t revPatch = 0;
+		std::uint32_t hashCommit = 0;
+
+		Utility::Mod::ModuleBase::moduleFieldReadIter<Utility::Mod::Module::Autopilot,
+			Utility::Mod::Fld::Field::VersionSoftwareMajor>([&revMajor](unsigned a){revMajor = a;});
+		Utility::Mod::ModuleBase::moduleFieldReadIter<Utility::Mod::Module::Autopilot,
+			Utility::Mod::Fld::Field::VersionSoftwareMinor>([&revMinor](unsigned a){revMinor = a;});
+		Utility::Mod::ModuleBase::moduleFieldReadIter<Utility::Mod::Module::Autopilot,
+			Utility::Mod::Fld::Field::VersionSoftwarePatch>([&revPatch](unsigned a){revPatch = a;});
+		Utility::Mod::ModuleBase::moduleFieldReadIter<Utility::Mod::Module::Autopilot,
+			Utility::Mod::Fld::Field::VersionCommitHash>([&hashCommit](unsigned a){hashCommit = a;});
+
+		const auto versionStringLength = snprintf(nullptr, 0, "%d.%d.%d-%x", revMajor, revMinor, revPatch, hashCommit);
+		char versionString[versionStringLength + 1] = {0};
+		snprintf(versionString, versionStringLength + 1, "%d.%d.%d-%x", revMajor, revMinor, revPatch, hashCommit);
+		cJSON_AddItemToArray(versions, cJSON_CreateString(versionString));  // Add AP's version to the JSON object
 	}
 
-	char *json = cJSON_Print(versions);
-	esp_err_t res = httpd_resp_send(req, json, strlen(json));
+	char *json = cJSON_Print(versions);  // Compose JSON string
+	esp_err_t res = httpd_resp_send(req, json, strlen(json));  // Send HTTP response
 
 	free(json);
 	cJSON_Delete(versions);
