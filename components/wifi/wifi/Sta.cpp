c2166dd7e8cb0f86e3b34fd3098608a94787de7e
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-09 13:28:26 +0300

    [wifi:Sta] Enh | Debug logging

--- components/wifi/wifi/Sta.cpp
+++ components/wifi/wifi/Sta.cpp
@@ -5,8 +5,17 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
+// Override debug level.
+// https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html#_CPPv417esp_log_level_setPKc15esp_log_level_t
+#define LOG_LOCAL_LEVEL ((esp_log_level_t)CONFIG_WIFI_DEBUG_LEVEL)
+#include <esp_log.h>
+
 #include "Sta.hpp"
+#include "wifi.h"
+#include "utility/LogSection.hpp"
 #include <esp_wifi.h>
+
+GS_UTILITY_LOGD_METHOD_SET_ENABLED(Wifi::Sta, getFieldValue, 1)
 
 namespace Wifi {
 
@@ -18,9 +27,9 @@ Sta::Sta() : Utility::Mod::ModuleBase{kModule}
 
 void Sta::getFieldValue(Utility::Mod::Fld::Req aReq, Utility::Mod::Fld::OnResponseCallback aOnResponse)
 {
-
 	switch (aReq.field) {
 		case Utility::Mod::Fld::Field::Initialized: {
+			GS_UTILITY_LOGD_METHOD(Wifi::kDebugTag, Sta, getFieldValue, "Field::Initialized");
 			wifi_ap_record_t wifiApRecord{};
 			aOnResponse(makeResponse<kModule, Utility::Mod::Fld::Field::Initialized>(
 				ESP_OK == esp_wifi_sta_get_ap_info(&wifiApRecord)));
