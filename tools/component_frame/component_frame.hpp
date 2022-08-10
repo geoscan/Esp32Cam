896ce837abd9faaf64e5663dacf07ec3978a2811
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-10 14:50:33 +0300

    [component_frame:CMakeLists,Kconfig,component_frame] Build | Component template

---
+++ tools/component_frame/component_frame.hpp
@@ -0,0 +1,13 @@
+//
+// COMPONENT_FRAME.hpp
+//
+// Created:
+//  Author:
+//
+
+namespace COMPONENT_FRAME_NAMESPACE {
+
+void init();
+static constexpr const char *kDebugTag = "[COMPONENT_FRAME]";
+
+}  // namespace COMPONENT_FRAME_NAMESPACE
