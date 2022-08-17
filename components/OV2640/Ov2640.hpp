e364edd064da0171a9fcb60539ef2a852607bfa1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-17 17:13:14 +0300

    [OV2640:Ov2640] Ref | Blank lines

--- components/OV2640/Ov2640.hpp
+++ components/OV2640/Ov2640.hpp
@@ -18,19 +18,18 @@ public:
 	Ov2640();
 	void init() override;
 	std::shared_ptr<Cam::Frame> getFrame() override;
+
 private:
+
 	class Frame : public Cam::Frame {
 		camera_fb_t *fb;
 	public:
 		Frame &operator=(const Frame &) = delete;
 		Frame(const Frame &) = delete;
-
 		Frame(Frame &&);
 		Frame &operator=(Frame &&);
-
 		Frame(camera_fb_t *);
 		~Frame();
-
 		int width() override;
 		int height() override;
 	};
