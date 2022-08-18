21ca41d6e05ffaf2e699de8ec5b290836e0ce9f1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-08-18 18:19:55 +0300

    [utility:sys,NvsWrap] Impl | NVS wrapper

---
+++ components/utility/utility/system/sys.hpp
@@ -0,0 +1,19 @@
+//
+// sys.hpp
+//
+// Created on: Aug 18, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(UTILITY_UTILITY_SYSTEM_SYS_HPP_)
+#define UTILITY_UTILITY_SYSTEM_SYS_HPP_
+
+namespace Utility {
+namespace Sys {
+
+static constexpr const char *kTag = "[sys]";
+
+}  // namespace Sys
+}  // namespace Utility
+
+#endif // UTILITY_UTILITY_SYSTEM_SYS_HPP_
