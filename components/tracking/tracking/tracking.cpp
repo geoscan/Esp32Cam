4cc0def53b07c3e75ebb387904a4c8ae4f030243
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-30 17:23:04 +0300

    [tracking:tracking] Impl | `profileInit`

--- components/tracking/tracking/tracking.cpp
+++ components/tracking/tracking/tracking.cpp
@@ -9,8 +9,10 @@
 // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html#_CPPv417esp_log_level_setPKc15esp_log_level_t
 #define LOG_LOCAL_LEVEL ((esp_log_level_t)CONFIG_TRACKING_DEBUG_LEVEL)
 #include <esp_log.h>
-
 #include "tracking.hpp"
+#include "Mosse.hpp"
+#include "Profile.hpp"
+#include <esp_log.h>
 
 namespace Trk {
 
@@ -19,6 +21,11 @@ void init()
 	esp_log_level_set(Trk::kDebugTag, (esp_log_level_t)CONFIG_TRACKING_DEBUG_LEVEL);
 	ESP_LOGD(Trk::kDebugTag, "Debug log test");
 	ESP_LOGV(Trk::kDebugTag, "Verbose log test");
+}
+
+void profileInit()
+{
+	static Profile profile;
 }
 
 }  // namespace Trk
