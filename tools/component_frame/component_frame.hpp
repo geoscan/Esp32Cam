8c2431075e0168a2c3620c0fd053db0095df004f
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-30 13:33:27 +0300

    [tools:component_frame] Build | Autogeneration: include guards

--- tools/component_frame/component_frame.hpp
+++ tools/component_frame/component_frame.hpp
@@ -5,9 +5,14 @@
 //  Author:
 //
 
+#if !defined(COMPONENTS_COMPONENT_FRAME_UPPER_HPP_)
+#define COMPONENTS_COMPONENT_FRAME_UPPER_HPP_
+
 namespace COMPONENT_FRAME_NAMESPACE {
 
 void init();
 static constexpr const char *kDebugTag = "[COMPONENT_FRAME]";
 
 }  // namespace COMPONENT_FRAME_NAMESPACE
+
+#endif  // COMPONENTS_COMPONENT_FRAME_UPPER_HPP_
