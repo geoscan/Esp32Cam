a1223793c42da060b48d38f76cd28c22200ca702
DmitryMurashov <dmtr.murashov@gmail.com>
2022-09-30 13:38:17 +0300

    [tracking:tracking] Impl | Carcass for the "tracking" module

---
+++ components/tracking/tracking/tracking.hpp
@@ -0,0 +1,18 @@
+//
+// tracking.hpp
+//
+// Created: 2022-09-30
+//  Author:
+//
+
+#if !defined(COMPONENTS_TRACKING_HPP_)
+#define COMPONENTS_TRACKING_HPP_
+
+namespace Trk {
+
+void init();
+static constexpr const char *kDebugTag = "[tracking]";
+
+}  // namespace Trk
+
+#endif  // COMPONENTS_TRACKING_HPP_
