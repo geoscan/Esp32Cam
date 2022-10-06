fe80e7f9995120474e994c46a962f78637a0f3ae
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-06 14:53:05 +0300

    [camera_thread:camera_thread] Build | Configurable core pinning, named task

--- components/camera_thread/camera_thread.cpp
+++ components/camera_thread/camera_thread.cpp
@@ -8,13 +8,18 @@
 #include "CameraStream.hpp"
 #include "camera_thread/camera_thread.hpp"
 #include "utility/thr/Threading.hpp"
+#include <sdkconfig.h>
 
 using namespace CameraThread;
 
+static void cameraThreadTask(void *)
+{
+	static CameraStream cameraStream;
+}
+
 void cameraThreadInit()
 {
-		static CameraStream cameraStream;
-
-		Ut::Thr::setThreadCoreAffinity(1);
-		static std::thread threadCameraStream(&CameraStream::operator(), &cameraStream);
+	static TaskHandle_t taskHandle;
+	xTaskCreatePinnedToCore(cameraThreadTask, "camera_thread", CONFIG_PTHREAD_STACK_MIN, 0,
+		Ut::Thr::FreertosTask::PriorityLowest, &taskHandle, CONFIG_CAMERA_THREAD_PIN_TO_CORE);
 }
