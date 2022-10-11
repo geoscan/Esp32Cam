479a49eae64bf036c69d36019d36120b21e9f7fe
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-11 17:43:19 +0300

    [wq:TaskVariant] Impl | Integrated TaskVariant

--- components/utility/utility/thr/wq/TaskVariant.cpp
+++ components/utility/utility/thr/wq/TaskVariant.cpp
@@ -14,67 +14,25 @@ namespace Ut {
 namespace Thr {
 namespace Wq {
 
-TaskVariant::TaskVariant(ContinuousTask &&aTask) : type{Type::ContinuousTask}
+TaskVariant::TaskVariant(Task &&aTask) : task{std::move(aTask)}
 {
-	new (storage.storage) ContinuousTask{aTask};
-}
-
-TaskVariant::TaskVariant(Task &&aTask) : type{Type::Task}
-{
-	new (storage.storage) Task{aTask};
 }
 
 TaskVariant::~TaskVariant()
 {
-	destructImpl();
 }
 
 /// \brief The task will get re-invoked for as long as it returns true.
 bool TaskVariant::operator()()
 {
-	switch (type) {
-		case Type::Task:
-			storage.asTask()();
+	task();
 
-			return false;
-
-		case Type::ContinuousTask:
-			return storage.asContinuousTask()();
-
-		case Type::Uninit:
-			return false;
-
-		default:
-			return false;
-	}
+	return false;
 }
 
 void TaskVariant::moveImpl(TaskVariant &&aTask)
 {
-	destructImpl();
-	type = aTask.type;
-	std::copy_n(aTask.storage.storage, sizeof(storage.storage), storage.storage);
-	aTask.type = Type::Uninit;
-}
-
-void TaskVariant::destructImpl()
-{
-	switch (type) {
-		case Type::Task:
-			storage.asTask().~Task();
-
-			break;
-
-		case Type::ContinuousTask:
-			storage.asContinuousTask().~ContinuousTask();
-
-			break;
-
-		case Type::Uninit:
-			break;
-	}
-
-	type = Type::Uninit;
+	task = std::move(aTask.task);
 }
 
 }  // namespace Wq
