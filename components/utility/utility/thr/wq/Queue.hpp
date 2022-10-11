b5c6c20a4d174ca01a8dd07730d0bb77a965d10a
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-11 11:15:10 +0300

    [wq:Queue] Ref | Moved `Wq::Queue` into separate file

---
+++ components/utility/utility/thr/wq/Queue.hpp
@@ -0,0 +1,34 @@
+//
+// Queue.hpp
+//
+// Created on: Oct 11, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(UTILITY_UTILITY_THR_WQ_QUEUE)
+#define UTILITY_UTILITY_THR_WQ_QUEUE
+
+#include "utility/thr/wq/Types.hpp"
+#include <mutex>
+#include <deque>
+
+namespace Ut {
+namespace Thr {
+namespace Wq {
+
+class Queue {
+public:
+	Queue();
+	void push(Task &&aTask);
+	bool pop(Task &aTask);
+private:
+	using QueueType = std::deque<Task>;
+	QueueType queue;
+	std::mutex mutex;
+};
+
+}  // namespace Wq
+}  // namespace Thr
+}  // namespace Ut
+
+#endif // UTILITY_UTILITY_THR_WQ_QUEUE
