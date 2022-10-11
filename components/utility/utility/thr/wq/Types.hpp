a91f5c2887051cd2d6ce6f03047e4c4a6aad57aa
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-11 10:57:23 +0300

    [wq:Types] Ref | Moved types into a separate header

---
+++ components/utility/utility/thr/wq/Types.hpp
@@ -0,0 +1,24 @@
+//
+// Types.hpp
+//
+// Created on: Oct 11, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(UTILITY_UTILITY_THR_WQ_TYPES_HPP_)
+#define UTILITY_UTILITY_THR_WQ_TYPES_HPP_
+
+#include <functional>
+
+namespace Ut {
+namespace Thr {
+namespace Wq {
+
+using Task = std::function<void()>;  ///< Regular tasks are removed from the queue after they are invoked.
+using ContinuousTask = std::function<bool()>;  ///< Continuous tasks are kept invoked iteratively for as long as they return true
+
+}  // namespace Wq
+}  // namespace Thr
+}  // namespace Ut
+
+#endif // UTILITY_UTILITY_THR_WQ_TYPES_HPP_
