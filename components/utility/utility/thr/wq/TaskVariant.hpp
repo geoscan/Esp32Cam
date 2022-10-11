0f281a3c864bcdeb366e02728e28e49991e73589
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-11 17:43:19 +0300

    [wq:TaskVariant] Impl | Integrated TaskVariant

--- components/utility/utility/thr/wq/TaskVariant.hpp
+++ components/utility/utility/thr/wq/TaskVariant.hpp
@@ -15,30 +15,8 @@ namespace Thr {
 namespace Wq {
 
 class TaskVariant final {
-private:
-	union Variant {
-		Task task;
-		ContinuousTask continuousTask;
-	};
-	enum class Type {
-		Uninit,
-		Task,
-		ContinuousTask,
-	};
-	struct Storage {
-		alignas(long int) std::uint8_t storage[sizeof(Variant)];
-
-		inline Task &asTask()
-		{
-			return *reinterpret_cast<Task *>(storage);
-		}
-		inline ContinuousTask &asContinuousTask()
-		{
-			return *reinterpret_cast<ContinuousTask *>(storage);
-		}
-	};
 public:
-	inline TaskVariant() : type{Type::Uninit}
+	inline TaskVariant()
 	{
 	}
 	inline TaskVariant(TaskVariant &&aTask)
@@ -51,12 +29,7 @@ public:
 
 		return *this;
 	}
-	inline bool isValid() const
-	{
-		return type != Type::Uninit;
-	}
 
-	TaskVariant(ContinuousTask &&aTask);
 	TaskVariant(Task &&aTask);
 	TaskVariant(const TaskVariant &) = delete;
 	TaskVariant &operator=(const TaskVariant &) = delete;
@@ -64,11 +37,8 @@ public:
 	bool operator()();
 private:
 	void moveImpl(TaskVariant &&);
-	void destructImpl();
 private:
-private:
-	Type type;
-	alignas(long int) Storage storage;
+	Task task;
 };
 
 }  // namespace Wq
