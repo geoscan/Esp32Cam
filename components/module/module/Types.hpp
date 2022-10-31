a4c4e83daee326e0f2e6079f2c508d7e68096893
DmitryMurashov <dmtr.murashov@gmail.com>
2022-10-31 10:36:56 +0300

    [module:Types] Impl | Types for the tracking algorithm

--- components/module/module/Types.hpp
+++ components/module/module/Types.hpp
@@ -19,10 +19,20 @@
 
 namespace Mod {
 
+/// \brief Enumeration for modules, i.e. semantically coherent pieces of functionality
 enum class Module : std::uint8_t {
 	Camera,
 	Autopilot,  ///< The main (autopilot) board
 	WifiStaConnection,  ///< Connection info (the ESP32 is connected to some other Access Point, AP)
+
+	/// \brief Tracking algorithm fields
+	///
+	/// \details
+	/// Fields, semantics:
+	/// - Initialized - set FALSE to disable tracking.
+	/// - Roi - bounding rectangle. Set to activate or reset tracking algorithm.
+	Tracking,
+
 	All,  ///< The request is addressed to every module
 };
 
@@ -30,6 +40,10 @@ struct None {};
 
 namespace Fld {
 
+/// \brief Enumerations for modules' fields.
+///
+/// \details Different modules may share similar fields. A field, and the type associated with it are uniquely
+/// identified by (Module, Field) pair.
 enum class Field : std::uint8_t {
 	FrameSize,
 	FrameFormat,  ///< Frame format currently used by a camera (e.g. JPEG)
@@ -43,13 +57,13 @@ enum class Field : std::uint8_t {
 	VersionSoftwarePatch,  ///< Software version, patch revision, or the number of commits from the latest revision,
 	VersionCommitHash,  ///< Git commit hash
 	Ip,  ///< Depends on context. Module=WifiStaConnection - host ip
+	Roi,  ///< Region of Interest
 };
 
 template <class T>
 using StoreType = typename Rr::Trait::StoreType<T>;
 
 /// \brief Compile-time selector of respective field types
-///
 template <Field, Module=Module::All> struct GetType : StoreType<None> {};  ///< Type selector
 template <> struct GetType<Field::FrameSize, Module::Camera> : StoreType<std::pair<int, int>> {};
 template <Module I> struct GetType<Field::Initialized, I> : StoreType<bool> {};
@@ -63,12 +77,18 @@ template <Module I> struct GetType<Field::VersionSoftwarePatch, I> : StoreType<u
 template <Module I> struct GetType<Field::VersionCommitHash, I> : StoreType<unsigned> {};
 template <Module I> struct GetType<Field::Ip, I> : StoreType<mapbox::util::variant<asio::ip::address_v4>> {};
 template <> struct GetType<Field::FrameFormat, Module::Camera> : StoreType<std::tuple<std::uint8_t, const char *>> {};  ///< (identifier, human-readable name)
+template <> struct GetType<Field::Roi, Module::Tracking> : StoreType<std::array<std::uint16_t, 4>> {};  ///< Rect: (x, y, width, height)
 
+/// \brief Compile-time selector of field types for set operations.
+///
+/// \details Enables overriding of field types for get and set operations.
 template <Field F, Module M = Module::All> struct SetType : GetType<F, M> {};
 template <> struct SetType<Field::FrameFormat, Module::Camera> : StoreType<const char *> {};
 
+/// \brief Variant type operating as the storage for the aforementioned types.
 using FieldVariantBase = typename mapbox::util::variant< None, unsigned, std::pair<int, int>, bool, const char *,
-	mapbox::util::variant<asio::ip::address_v4>, std::tuple<std::uint8_t, const char *>, std::uint8_t>;
+	mapbox::util::variant<asio::ip::address_v4>, std::tuple<std::uint8_t, const char *>, std::uint8_t,
+	std::array<std::uint16_t, 4>>;
 
 struct FieldVariant : public FieldVariantBase {
 	using FieldVariantBase::FieldVariantBase;
