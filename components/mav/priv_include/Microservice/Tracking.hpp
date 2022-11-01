d52e2baefead358078dfc0464e1d39c6069d75c4
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-01 17:02:51 +0300

    [mav:Tracking] Ref | Comments

--- components/mav/priv_include/Microservice/Tracking.hpp
+++ components/mav/priv_include/Microservice/Tracking.hpp
@@ -17,6 +17,29 @@
 namespace Mav {
 namespace Mic {
 
+/// \brief Provides MAVLink API to the tracking functionality.
+///
+/// \details MAVLink protocol provides relevant messages for interfacing w/ a tracking implementor. However, as per
+/// 2022-11-01, its API is unstable (WIP), and its use is discrouraged.
+///
+/// The following messages are used for tracking control:
+///
+/// MAV_CMD_SET_MESSAGE_INTERVAL (https://mavlink.io/en/messages/common.html#MAV_CMD_SET_MESSAGE_INTERVAL)
+/// Parameters:
+/// `param 1` - Message ID - id of the message. Should be 250 (DEBUG_VECT)
+/// `param 2` - Interval.
+///    - `-1` for disabling the tracker
+///    - `0` for sending the message on a per-frame basis
+///
+/// DEBUG_VECT(https://mavlink.io/en/messages/common.html#DEBUG_VECT)
+/// `time_usec` - as per protocol
+/// `x` - packed field containing 2 x u16 values
+///   - bits [0; 15] - ROI center, x
+///   - bits [16; 32] - frame size, x
+/// 'y' - packed field containing 2 x u16 values
+///   - bits [0; 15] - ROI center, y
+///   - bits [16, 31] - frame size, y
+/// 'z' - PSR value (Peak-to-Sidelobe ratio, google MOSSE tracker)
 class Tracking final : public Microservice, public Ut::Sys::HrTimer, public Mav::DelayedSend {
 private:
 	/// \brief Event keys
