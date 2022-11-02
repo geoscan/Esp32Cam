4c1d00d87f1f7f1ff23ae68171256ed1a2208c9c
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-02 16:00:33 +0300

    [al:Algorithm] Impl | `scale()`

--- components/utility/utility/al/Algorithm.hpp
+++ components/utility/utility/al/Algorithm.hpp
@@ -31,9 +31,20 @@ bool in(const T &object, const Args &...values)
 template <typename T, typename O = float>
 O normalize(T val, T from, T to)
 {
-	assert(to - from > std::numeric_limits<float>::epsilon());
+	assert(to - from > std::numeric_limits<T>::epsilon());
 
 	return static_cast<O>(val - from) / static_cast<O>(to - from);
+}
+
+/// \brief De-normalizes the value
+/// \tparam T Input value type
+/// \tparam O Output value type
+template <class T, class O>
+O scale(T val, O from, O to)
+{
+	assert(to - from > std::numeric_limits<T>::epsilon());
+
+	return static_cast<O>((static_cast<T>(to) - static_cast<T>(from)) * val + static_cast<T>(from));
 }
 
 /// \brief Fits the value in a specified range
