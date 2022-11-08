aeaf7980b08699cebc97d74e020c003a7b23d4ce
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-08 13:28:27 +0300

    [mav:Tracking] Opt | Duplicating functionality

--- components/mav/priv_include/Microservice/Tracking.cpp
+++ components/mav/priv_include/Microservice/Tracking.cpp
@@ -131,32 +131,24 @@ Microservice::Ret Tracking::processCmdCameraTrackRectangle(mavlink_command_long_
 			&& aMavlinkCommandLong.param3 >= 0.0f
 			&& aMavlinkCommandLong.param4 >= 0.0f)) {
 		ESP_LOGW(Mav::kDebugTag, "Tracking: failed geometry check");
+		result = MAV_RESULT_DENIED;
 	} else {
-
-	}
-
-	if (!cameraState.isInitialized()) {
-		cameraState.fetch();
-		// TODO: process fetch failure
-		const auto topLeftX = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param1, 0,
-			cameraState.frameWidth));
-		const auto topLeftY = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param2, 0,
-			cameraState.frameHeight));
-		const auto bottomRightX = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param3, 0,
-			cameraState.frameWidth));
-		const auto bottomRightY = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param4, 0,
-			cameraState.frameHeight));
-		std::array<std::uint16_t, 4> rect = {{topLeftX, topLeftY, static_cast<std::uint16_t>(bottomRightX - topLeftX),
-			static_cast<std::uint16_t>(bottomRightY - topLeftY)}};
-		GS_UTILITY_LOGD_CLASS_ASPECT(Mav::kDebugTag, Tracking, "messaging",
-			"Tracking: left %d  top %d  right %d  bottom %d  frame height %d  frame width %d", topLeftX, topLeftY,
-			bottomRightX, bottomRightY, cameraState.frameHeight, cameraState.frameWidth);
-
-		if (!(topLeftX < bottomRightX && topLeftY < bottomRightY)) {
-			result = MAV_RESULT_DENIED;
-			ESP_LOGW(Mav::kDebugTag, "Tracking: failed geometry check left %d  top %d  right %d  bottom %d", topLeftX,
-				topLeftY, bottomRightX, bottomRightY);
-		} else {
+		if (!cameraState.isInitialized()) {
+			cameraState.fetch();
+			// TODO: process fetch failure
+			const auto topLeftX = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param1, 0,
+				cameraState.frameWidth));
+			const auto topLeftY = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param2, 0,
+				cameraState.frameHeight));
+			const auto bottomRightX = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param3, 0,
+				cameraState.frameWidth));
+			const auto bottomRightY = static_cast<std::uint16_t>(Ut::Al::scale(aMavlinkCommandLong.param4, 0,
+				cameraState.frameHeight));
+			std::array<std::uint16_t, 4> rect = {{topLeftX, topLeftY, static_cast<std::uint16_t>(bottomRightX - topLeftX),
+				static_cast<std::uint16_t>(bottomRightY - topLeftY)}};
+			GS_UTILITY_LOGD_CLASS_ASPECT(Mav::kDebugTag, Tracking, "messaging",
+				"Tracking: left %d  top %d  right %d  bottom %d  frame height %d  frame width %d", topLeftX, topLeftY,
+				bottomRightX, bottomRightY, cameraState.frameHeight, cameraState.frameWidth);
 			Mod::ModuleBase::moduleFieldWriteIter<Mod::Module::Tracking, Mod::Fld::Field::Roi>(rect,
 				[&result](Mod::Fld::WriteResp aResp) mutable
 				{
