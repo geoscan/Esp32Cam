7dbb48a8109c58903dcd472e359583cd015b5202
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-09 12:51:15 +0300

    [mav:DelayedSend] Impl | Async notifications' context

--- components/mav/priv_include/DelayedSend.hpp
+++ components/mav/priv_include/DelayedSend.hpp
@@ -10,8 +10,34 @@
 
 #include "Mavlink.hpp"
 #include "utility/Subscription.hpp"
+#include "mapbox/variant.hpp"
 
 namespace Mav {
+
+using DelayedSendAsyncVariant = typename mapbox::util::variant<mavlink_camera_tracking_image_status_t>;
+
+/// \brief Enables memory-efficient asynchronous messaging
+///
+/// \details Notification of MAVLink receivers (i.e. interfaces) is achieved throught a work queue w/ a subsequent
+/// waitiing for the notification sequence to finish. Thus, calling notify in the context of a work queue task produces
+/// dead lock, because the work queue becomes unable to proceeed due to active waiting.
+///
+/// The alternative is to notify in an asynchronous manner, without waiting, but it'd require copying of data. To spare
+/// ourselves the memory expenses, the payload is thus stored as a compact mavlink structure.
+struct DelayedSendAsyncCtx {
+	/// System id (the one that is used in MAVLink messages, http://mavlink.io/en/guide/serialization.html)
+	std::uint8_t sysId;
+	/// Component id
+	std::uint8_t compId;
+	/// MAVGen-generated struct
+	DelayedSendAsyncVariant delayedSendAsyncVariant;
+
+	template <class ...Ts>
+	DelayedSendAsyncCtx(std::uint8_t aSysId, std::uint8_t aCompId, Ts &&...aArgs) : sysId{aSysId}, compId{aCompId},
+		delayedSendAsyncVariant{std::forward<Ts>(aArgs)...}
+	{
+	}
+};
 
 class DelayedSendHandle {
 public:
@@ -19,10 +45,8 @@ public:
 	virtual ~DelayedSendHandle() = default;
 };
 
-///
 /// \brief Enables one to send a MAVLink message outside the request/response scheme. Useful for implementing telemetry
 /// messages sending
-///
 using DelayedSend = Ut::Sub::Sender1to1<DelayedSendHandle>;
 
 }  // namespace Mav
