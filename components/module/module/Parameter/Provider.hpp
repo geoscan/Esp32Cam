70811f9c711ec78baa271a366c8da3581baba423
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-24 15:52:25 +0300

    [module:Provider] Impl | Bare type definition

---
+++ components/module/module/Parameter/Provider.hpp
@@ -0,0 +1,21 @@
+//
+// Provider.hpp
+//
+// Created on: Nov 24, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MODULE_MODULE_PARAMETER_PROVIDER_HPP_)
+#define MODULE_MODULE_PARAMETER_PROVIDER_HPP_
+
+namespace Mod {
+namespace Par {
+
+/// \brief Provides an API to a non-volatile storage (SD card or NVS)
+class Provider {
+};
+
+}  // namespace Par
+}  // namespace Mod
+
+#endif // MODULE_MODULE_PARAMETER_PROVIDER_HPP_
