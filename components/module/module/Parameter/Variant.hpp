7e5040f95666d4dd2a1436d88089092b2efa9b41
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-24 16:41:36 +0300

    [module:Variant] Impl | Bare class declaration

---
+++ components/module/module/Parameter/Variant.hpp
@@ -0,0 +1,20 @@
+//
+// Variant.hpp
+//
+// Created on: Nov 24, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MODULE_MODULE_PARAMETER_PARAMETERVALUESTORAGE_HPP_)
+#define MODULE_MODULE_PARAMETER_PARAMETERVALUESTORAGE_HPP_
+
+namespace Mod {
+namespace Par {
+
+class Variant {
+};
+
+}  // namespace Par
+}  // namespace Mod
+
+#endif // MODULE_MODULE_PARAMETER_PARAMETERVALUESTORAGE_HPP_
