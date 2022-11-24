c0a3952cfc0839656f39a194703591506a22cdb1
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-24 16:57:29 +0300

    [module:MemoryProvider] Impl | Defined load / Store API

--- components/module/module/Parameter/MemoryProvider.hpp
+++ components/module/module/Parameter/MemoryProvider.hpp
@@ -8,11 +8,21 @@
 #if !defined(MODULE_MODULE_PARAMETER_PROVIDER_HPP_)
 #define MODULE_MODULE_PARAMETER_PROVIDER_HPP_
 
+#include "module/Parameter/Result.hpp"
+
 namespace Mod {
 namespace Par {
 
+class ParameterDescription;
+class Variant;
+
 /// \brief Provides an API to a non-volatile storage (SD card or NVS)
 class MemoryProvider {
+public:
+	/// \brief Make an attempt to load value from a non-volatile storage
+	virtual Result load(const ParameterDescription &, Variant &) = 0;
+	/// \brief Make an attempt to store value into a non-volatile storage
+	virtual Result save(const ParameterDescription &, const Variant &value) = 0;
 };
 
 }  // namespace Par
