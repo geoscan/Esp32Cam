1770c3cd4b8cdf7f9ae3488d7d49cc764f31cbfa
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-24 16:55:34 +0300

    [module:Result] Impl

---
+++ components/module/module/Parameter/Result.hpp
@@ -0,0 +1,22 @@
+//
+// Result.hpp
+//
+// Created on: Nov 24, 2022
+//     Author: Dmitry Murashov (d.murashov@geoscan.aero)
+//
+
+#if !defined(MODULE_MODULE_PARAMETER_RESULT_HPP_)
+#define MODULE_MODULE_PARAMETER_RESULT_HPP_
+
+namespace Mod {
+namespace Par {
+
+enum class Result {
+	/// No error has occured
+	Ok,
+};
+
+}  // namespace Par
+}  // namespace Mod
+
+#endif // MODULE_MODULE_PARAMETER_RESULT_HPP_
