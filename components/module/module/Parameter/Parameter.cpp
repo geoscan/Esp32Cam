3b259ca094d01365ffdf1e5a75e6b66fdbf31639
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-25 12:24:39 +0300

    [module:Parameter] Ref | Grouping

--- components/module/module/Parameter/Parameter.cpp
+++ components/module/module/Parameter/Parameter.cpp
@@ -14,39 +14,40 @@
 namespace Mod {
 namespace Par {
 
-static constexpr std::array<ParameterDescription, 1> kParameterDescriptions{{
+struct ParameterDescriptionStorage {
+	static constexpr std::array<ParameterDescription, 1> kParameterDescriptions{{
+		{
+			0,  // id
+			"WifiApSsid",  // name
+			ParameterType::Str,
+			Mod::Module::WifiAp,
+			Mod::Fld::Field::StringIdentifier,
+		}
+	}};
+	/// \brief Max length of a parameter's string name. It is kept being equal 16, for compatibility w/ MAVLink
+	static constexpr std::size_t kParameterStrlen = 16;
+	/// \brief Compile-time validation of parameters' name length
+	static constexpr bool validateStrlen(std::size_t pos = 0)
 	{
-		0,  // id
-		"WifiApSsid",  // name
-		ParameterType::Str,
-		Mod::Module::WifiAp,
-		Mod::Fld::Field::StringIdentifier,
+		return pos >= kParameterDescriptions.size() ?
+			true :
+			Ut::Al::Str::cxStrlen(kParameterDescriptions[pos].name) <= kParameterStrlen && validateStrlen(pos + 1);
 	}
-}};
+};
 
-/// \brief Max length of a parameter's string name. It is kept being equal 16, for compatibility w/ MAVLink
-constexpr std::size_t kParameterStrlen = 16;
-
-/// \brief Compile-time validation of parameters' name length
-constexpr bool pdescValidateStrlen(std::size_t pos = 0)
-{
-	return pos >= kParameterDescriptions.size() ?
-		true :
-		Ut::Al::Str::cxStrlen(kParameterDescriptions[pos].name) <= kParameterStrlen && pdescValidateStrlen(pos + 1);
-}
-
-static_assert(pdescValidateStrlen(), "A parameter's name length has been exceeded");
+static_assert(ParameterDescriptionStorage::validateStrlen(), "A parameter's name length has been exceeded");
 
 bool Parameter::toId(Module module, Fld::Field field, std::size_t &oId)
 {
 	bool res = false;
-	auto it = std::find_if(std::begin(kParameterDescriptions), std::end(kParameterDescriptions),
+	auto it = std::find_if(std::begin(ParameterDescriptionStorage::kParameterDescriptions),
+		std::end(ParameterDescriptionStorage::kParameterDescriptions),
 		[module, field](const ParameterDescription &aDescription)
 		{
 			return module == aDescription.module && field == aDescription.field;
 		});
 
-	if (it != std::end(kParameterDescriptions)) {
+	if (it != std::end(ParameterDescriptionStorage::kParameterDescriptions)) {
 		res = true;
 		oId = it->id;
 	}
