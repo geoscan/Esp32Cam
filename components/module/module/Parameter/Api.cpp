31cae09fa77c56cbe0c5208480a8d40e8fe48a96
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-25 10:16:06 +0300

    [module:Api] Impl | Constexpr parameters' name length validation

--- components/module/module/Parameter/Api.cpp
+++ components/module/module/Parameter/Api.cpp
@@ -6,6 +6,7 @@
 //
 
 #include "module/Parameter/ParameterDescription.hpp"
+#include "utility/al/String.hpp"
 #include <array>
 #include "Api.hpp"
 
@@ -14,13 +15,26 @@ namespace Par {
 
 static constexpr std::array<ParameterDescription, 1> kParameterDescriptions{{
 	{
+		0,  // id
 		"WifiApSsid",  // name
 		ParameterType::Str,
-		0,  // id
 		Mod::Module::WifiAp,
 		Mod::Fld::Field::StringIdentifier,
 	}
 }};
+
+/// \brief Max length of a parameter's string name. It is kept being equal 16, for compatibility w/ MAVLink
+constexpr std::size_t kParameterMaxName = 16;
+
+/// \brief Compile-time validation of parameters' name length
+constexpr bool pdescValidateStrlen(std::size_t pos = 0)
+{
+	return pos >= kParameterDescriptions.size() ?
+		true :
+		Ut::Al::Str::cxStrlen(kParameterDescriptions[pos].name) <= kParameterMaxName && pdescValidateStrlen(pos + 1);
+}
+
+static_assert(pdescValidateStrlen(), "A parameter's name length has been exceeded");
 
 Api::Api() : Ut::MakeSingleton<Api>{*this}
 {
