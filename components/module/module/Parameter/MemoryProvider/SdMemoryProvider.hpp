5b6580834951962d424828a5cd47da1189997656
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-28 10:39:44 +0300

    [module:SdMemoryProvider] Ref | Comments

--- components/module/module/Parameter/MemoryProvider/SdMemoryProvider.hpp
+++ components/module/module/Parameter/MemoryProvider/SdMemoryProvider.hpp
@@ -22,7 +22,13 @@ public:
 	Result load(const ParameterDescription &, Variant &) override;
 	Result save(const ParameterDescription &, const Variant &value) override;
 private:
-	/// \brief Given that
+	/// \brief Performs filesystem and config file checks.
+	/// \post If the method results in success, the following may be relied
+	/// upon:
+	/// 1. The SD card has been mounted and initialized;
+	/// 2. The filesystem has been successfully attached;
+	/// 3. The config file is already present in the filesystem, or it has been
+	/// created.
 	static Result configFileEnsureExists();
 	/// \brief Reads the content of an entire config file into RAM buffer.
 	static Result configFileRead(std::unique_ptr<std::uint8_t[]> &buffer);
