b0b70ec816e45422f5a71a0f4ac9a076a6d44ab0
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-28 16:37:29 +0300

    [module:Parameter] Bug | Linker error
    Using `static constexpr std::array...` inside a structure definition causes
    linker error (undefined symbol). Apparently, there is some bug in Xtensa GCC.

--- components/module/module/Parameter/Parameter.cpp
+++ components/module/module/Parameter/Parameter.cpp
@@ -17,60 +17,53 @@
 namespace Mod {
 namespace Par {
 
-/// \brief Constexpr storage of parameter descriptions.
-struct ParameterDescriptionStorage {
-	static constexpr const std::array<ParameterDescription, 1> kParameterDescriptions = {{
+static constexpr const std::array<ParameterDescription, 1> kParameterDescriptions = {{
+	{
+		0,  // id
+		"WifiApSsid",  // name
+		ParameterType::Str,
+		Mod::Module::WifiAp,
+		Mod::Fld::Field::StringIdentifier,
+		MemoryProviderType::Sd,
+	}
+}};
+
+/// \brief Max length of a parameter's string name. It is kept being equal 16, for compatibility w/ MAVLink
+static constexpr std::size_t kParameterStrlen = 16;
+
+/// \brief Compile-time validation of parameters' name length
+static constexpr bool pardescValidateStrlen(std::size_t pos = 0)
+{
+	return pos >= kParameterDescriptions.size() ?
+		true :
+		Ut::Al::Str::cxStrlen(kParameterDescriptions[pos].name) <= kParameterStrlen && pardescValidateStrlen(pos + 1);
+}
+
+/// \brief Convert (MODULE, FIELD) pair to a parameter's unique identifier
+/// \returns True, if found. Sets `oId`
+static bool pardescToId(Module module, Fld::Field field, std::size_t &oId)
+{
+	bool res = false;
+	auto it = std::find_if(std::begin(kParameterDescriptions),
+		std::end(kParameterDescriptions),
+		[module, field](const ParameterDescription &aDescription)
 		{
-			0,  // id
-			"WifiApSsid",  // name
-			ParameterType::Str,
-			Mod::Module::WifiAp,
-			Mod::Fld::Field::StringIdentifier,
-			MemoryProviderType::Sd,
-		}
-	}};
+			return module == aDescription.module && field == aDescription.field;
+		});
 
-	/// \brief Max length of a parameter's string name. It is kept being equal 16, for compatibility w/ MAVLink
-	static constexpr std::size_t kParameterStrlen = 16;
-	/// \brief Compile-time validation of parameters' name length
-	static constexpr bool validateStrlen(std::size_t pos = 0)
-	{
-		return pos >= kParameterDescriptions.size() ?
-			true :
-			Ut::Al::Str::cxStrlen(kParameterDescriptions[pos].name) <= kParameterStrlen && validateStrlen(pos + 1);
+	if (it != std::end(kParameterDescriptions)) {
+		res = true;
+		oId = it->id;
 	}
 
-	/// \brief Convert (MODULE, FIELD) pair to a parameter's unique identifier
-	/// \returns True, if found. Sets `oId`
-	static bool toId(Module module, Fld::Field field, std::size_t &oId)
-	{
-		bool res = false;
-		auto it = std::find_if(std::begin(ParameterDescriptionStorage::kParameterDescriptions),
-			std::end(ParameterDescriptionStorage::kParameterDescriptions),
-			[module, field](const ParameterDescription &aDescription)
-			{
-				return module == aDescription.module && field == aDescription.field;
-			});
+	return res;
+}
 
-		if (it != std::end(ParameterDescriptionStorage::kParameterDescriptions)) {
-			res = true;
-			oId = it->id;
-		}
-
-		return res;
-	}
-
-	static std::size_t size()
-	{
-		return kParameterDescriptions.size();
-	}
-};
-
-static_assert(ParameterDescriptionStorage::validateStrlen(), "A parameter's name length has been exceeded");
+static_assert(pardescValidateStrlen(), "A parameter's name length has been exceeded");
 
 /// \brief Static parameter instances storage.
 struct InstanceStorage {
-	std::array<std::unique_ptr<Parameter>, ParameterDescriptionStorage::kParameterDescriptions.size()> instances;
+	std::array<std::unique_ptr<Parameter>, kParameterDescriptions.size()> instances;
 
 	/// \brief Makes an attempt to construct an instance. Returns, if already constructed
 	void ensureAt(std::size_t id)
@@ -121,8 +114,8 @@ struct MemoryProviderStorage {
 
 	MemoryProvider *memoryProviderById(std::size_t id)
 	{
-		if (id < ParameterDescriptionStorage::size()) {
-			switch (ParameterDescriptionStorage::kParameterDescriptions[id].memoryProviderType) {
+		if (id < kParameterDescriptions.size()) {
+			switch (kParameterDescriptions[id].memoryProviderType) {
 				case MemoryProviderType::Nvs:
 					return nvsMemoryProvider();
 
@@ -145,7 +138,7 @@ Result Parameter::fetch()
 	if (memoryProvider == nullptr) {
 		res = Result::MemoryProviderNotFound;
 	} else {
-		res = memoryProvider->load(ParameterDescriptionStorage::kParameterDescriptions[id()], *this);
+		res = memoryProvider->load(kParameterDescriptions[id()], *this);
 	}
 
 	return res;
@@ -159,7 +152,7 @@ Result Parameter::commit()
 	if (memoryProvider == nullptr) {
 		res = Result::MemoryProviderNotFound;
 	} else {
-		res = memoryProvider->save(ParameterDescriptionStorage::kParameterDescriptions[id()], *this);
+		res = memoryProvider->save(kParameterDescriptions[id()], *this);
 	}
 
 	return res;
@@ -170,7 +163,7 @@ Parameter *Parameter::instanceByMf(Module module, Fld::Field field)
 	std::size_t id;
 	Parameter *instance = nullptr;
 
-	if (ParameterDescriptionStorage::toId(module, field, id)) {
+	if (pardescToId(module, field, id)) {
 		instance = sInstanceStorage.instanceById(id);
 	}
 
