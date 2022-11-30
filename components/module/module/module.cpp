932cf0a1506209cc71b3c66426259b00764895fb
DmitryMurashov <dmtr.murashov@gmail.com>
2022-11-30 09:42:23 +0300

    [module:module] Impl | Demo

--- components/module/module/module.cpp
+++ components/module/module/module.cpp
@@ -2,7 +2,7 @@
 // module.hpp
 //
 // Created: 2022-08-10
-//  Author:
+//  Author: d.murashov
 //
 
 // Override debug level.
@@ -13,6 +13,7 @@
 #include "module/Parameter/MemoryProvider.hpp"
 #include "module/Parameter/MemoryProvider/SdMemoryProvider.hpp"
 #include "module/Parameter/Parameter.hpp"
+#include "utility/time.hpp"
 
 namespace Mod {
 
@@ -23,8 +24,22 @@ void initDemoPar()
 {
 	auto *instance = Par::Parameter::instanceByMf(Mod::Module::WifiAp, Mod::Fld::Field::StringIdentifier);
 	assert(instance != nullptr);
-	(*instance) = "EchoHello";
-	instance->commit();
+	auto res = instance->fetch();
+	ESP_LOGI(Mod::kDebugTag, "module: got instance, id %d", instance->id());
+
+	if (res == Mod::Par::Result::Ok) {
+		ESP_LOGI(Mod::kDebugTag, "module: initDemoPar: successfully fetched the parameter, value: %s",
+			instance->asStr().c_str());
+	} else {
+		ESP_LOGI(Mod::kDebugTag, "module: initDemoPar: failed to fetch the parameter, error %s",
+			Mod::Par::resultAsStr(res));
+		constexpr const char *kValue = "EchoHello";
+		ESP_LOGI(Mod::kDebugTag, "module: got instance, id %d", instance->id());
+		instance->set(kValue);
+		ESP_LOGI(Mod::kDebugTag, "module: got instance, id %d", instance->id());
+		ESP_LOGI(Mod::kDebugTag, "module: setting value %s, parameter id %d", kValue, instance->id());
+		instance->commit();
+	}
 }
 
 void init()
@@ -32,7 +47,6 @@ void init()
 	esp_log_level_set(Mod::kDebugTag, (esp_log_level_t)CONFIG_MODULE_DEBUG_LEVEL);
 	ESP_LOGD(Mod::kDebugTag, "Debug log test");
 	ESP_LOGV(Mod::kDebugTag, "Verbose log test");
-	initDemoPar();
 }
 
 }  // namespace Mod
