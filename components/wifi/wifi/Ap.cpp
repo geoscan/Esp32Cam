450e77eb8d9a0bf06d2489a1b03def0ab9e51837
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-02 11:05:17 +0300

    [wifi:Ap] Enh | Reliability: SSID length checking

--- components/wifi/wifi/Ap.cpp
+++ components/wifi/wifi/Ap.cpp
@@ -5,17 +5,42 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
+#include <esp_log.h>
 #include "Ap.hpp"
+#include "module/module.hpp"
 
 namespace Wifi {
+
+static constexpr std::size_t kSsidMinLength = 1;
+static constexpr std::size_t kSsidMaxLength = 32;
+static constexpr const char *kInvalidSsidLengthMsg = "Invalid SSID length";
 
 Ap::Ap() : ModuleBase{Mod::Module::WifiAp}
 {
 }
 
-void Ap::setFieldValue(Mod::Fld::WriteReq, Mod::Fld::OnWriteResponseCallback onWriteResponse)
+void Ap::setFieldValue(Mod::Fld::WriteReq request, Mod::Fld::OnWriteResponseCallback onWriteResponse)
 {
-	onWriteResponse({Mod::Fld::RequestResult::Ok});
+	Mod::Fld::WriteResp writeResp{Mod::Fld::RequestResult::Ok};
+
+	switch (request.field) {
+		case Mod::Fld::Field::StringIdentifier: {  // SSID
+			const std::string & ssid
+				= request.variant.getUnchecked<Mod::Module::WifiAp, Mod::Fld::Field::StringIdentifier>();
+
+			if (!(ssid.length() >= kSsidMinLength && ssid.length() <= kSsidMaxLength)) {
+				ESP_LOGW(Mod::kDebugTag, "set StringIdentifier: %s", kInvalidSsidLengthMsg);
+				writeResp = {Mod::Fld::RequestResult::Other, kInvalidSsidLengthMsg};
+			}
+
+			break;
+		}
+
+		default:
+			break;
+	}
+
+	onWriteResponse(writeResp);
 }
 
 }  // namespace Wifi
