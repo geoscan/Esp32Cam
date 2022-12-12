5e390b854388a0c4805cddb18514c334a4522210
DmitryMurashov <dmtr.murashov@gmail.com>
2022-12-12 11:28:38 +0300

    [wifi:Sta] Impl | Buffered credentials, Module API for connection activation

--- components/wifi/wifi/Sta.cpp
+++ components/wifi/wifi/Sta.cpp
@@ -13,15 +13,22 @@
 #include "Sta.hpp"
 #include "wifi.h"
 #include "utility/LogSection.hpp"
+#include "module/Parameter/Parameter.hpp"
 #include <esp_wifi.h>
 
 GS_UTILITY_LOGD_METHOD_SET_ENABLED(Wifi::Sta, getFieldValue, 1)
 
 namespace Wifi {
 
+constexpr std::size_t kSsidMinLength = 1;
+constexpr std::size_t kSsidMaxLength = 32;
+constexpr std::size_t kPasswordMinLength = 8;
+constexpr std::size_t kPasswordMaxLength = 64;
+
+
 static constexpr auto kModule = Mod::Module::WifiStaConnection;
 
-Sta::Sta(esp_netif_t **aEspNetif) : Mod::ModuleBase{kModule}, espNetif{aEspNetif}
+Sta::Sta(esp_netif_t **aEspNetif) : Mod::ModuleBase{kModule}, espNetif{aEspNetif}, credentials{"", ""}
 {
 }
 
@@ -51,6 +58,112 @@ void Sta::getFieldValue(Mod::Fld::Req aReq, Mod::Fld::OnResponseCallback aOnResp
 		default:
 			break;
 	}
+}
+
+void Sta::setFieldValue(Mod::Fld::WriteReq writeReq, Mod::Fld::OnWriteResponseCallback onResponse)
+{
+	Mod::Fld::WriteResp resp = {Mod::Fld::RequestResult::Ok};
+
+	if (writeReq.field == Mod::Fld::Field::Password) {
+		if (!credentials.trySetPassword(writeReq.variant.getUnchecked<Mod::Module::WifiStaConnection,
+				Mod::Fld::Field::StringIdentifier>())) {
+			resp = {Mod::Fld::RequestResult::Other, "Wrong password format"};
+		}
+
+		onResponse(resp);
+	} else if (writeReq.field == Mod::Fld::Field::StringIdentifier) {
+		if (!credentials.trySetSsid(writeReq.variant.getUnchecked<Mod::Module::WifiStaConnection,
+				Mod::Fld::Field::StringIdentifier>())) {
+			resp = {Mod::Fld::RequestResult::Other, "Wrong SSID format"};
+		}
+
+		onResponse(resp);
+	} else if (writeReq.field == Mod::Fld::Field::Initialized) {  // Activate connection
+		if (!credentials.isValid()) {
+			Mod::Par::Result result = credentials.fetch();
+
+			if (result != Mod::Par::Result::Ok || !credentials.isValid()) {
+				resp = {Mod::Fld::RequestResult::Other, Mod::Par::resultAsStr(result)};
+			}
+		}
+
+		if (resp.isOk()) {
+			constexpr std::uint8_t *kIp = nullptr;
+			constexpr std::uint8_t *kGateway = nullptr;
+			constexpr std::uint8_t *kNetmask = nullptr;
+			const esp_err_t espErr = wifiStaConnect(credentials.ssid.c_str(), credentials.password.c_str(), kIp,
+				kGateway, kNetmask);
+
+			if (espErr != ESP_OK) {
+				resp = {Mod::Fld::RequestResult::Other, esp_err_to_name(espErr)};
+			}
+		}
+
+		onResponse(resp);
+	}
+}
+
+Mod::Par::Result Sta::Credentials::fetch()
+{
+	Mod::Par::Result result = Mod::Par::Result::Ok;
+	auto *paramSsid = Mod::Par::Parameter::instanceByMf(Mod::Module::WifiStaConnection,
+		Mod::Fld::Field::StringIdentifier);
+
+	if (paramSsid != nullptr) {
+		result = paramSsid->fetch();
+
+		if (result == Mod::Par::Result::Ok) {
+			ssid = paramSsid->asStr();
+		}
+	} else {
+		result = Mod::Par::Result::ConfigDoesNotExist;
+	}
+
+	if (result == Mod::Par::Result::Ok) {
+		auto *paramPassword = Mod::Par::Parameter::instanceByMf(Mod::Module::WifiStaConnection,
+			Mod::Fld::Field::Password);
+
+		if (paramPassword != nullptr) {
+			result = paramPassword->fetch();
+
+			if (result == Mod::Par::Result::Ok) {
+				password = paramSsid->asStr();
+			}
+		} else {
+			result = Mod::Par::Result::ConfigDoesNotExist;
+		}
+	}
+
+	return result;
+}
+
+bool Sta::Credentials::isValid() const
+{
+	return ssid.length() >= kSsidMinLength && ssid.length() <= kSsidMaxLength && (password.length() == 0
+		|| (password.length() >= kPasswordMinLength && password.length() <= kPasswordMaxLength));
+}
+
+bool Sta::Credentials::trySetPassword(const std::string &password)
+{
+	bool ret = password.length() == 0 || (password.length() >= kPasswordMinLength
+		&& password.length() <= kPasswordMaxLength);
+
+	if (ret) {
+		this->password = password;
+	}
+
+	return ret;
+}
+
+bool Sta::Credentials::trySetSsid(const std::string &ssid)
+{
+	bool ret = ssid.length() >= kSsidMinLength && ssid.length() <= kSsidMaxLength;
+
+	if (ret) {
+		this->ssid = ssid;
+	}
+
+	return ret;
 }
 
 }  // namespace Wifi
--- components/wifi/wifi/Sta.hpp
+++ components/wifi/wifi/Sta.hpp
@@ -6,17 +6,35 @@
 //
 
 #include "module/ModuleBase.hpp"
+#include "module/Parameter/Result.hpp"
 #include <esp_netif.h>
+#include <string>
 
 namespace Wifi {
 
+/// \brief `Sta` class provides module API for Wi-Fi STA-related functionality
 class Sta : public Mod::ModuleBase {
+private:
+	/// \brief Buffered connection credentials
+	struct Credentials {
+		/// SSID of the AP the STA should connect to
+		std::string ssid;
+		/// Password of the AP the STA should connect to
+		std::string password;
+		/// \brief Makes an attempt to fetch the parameters from non-volatile
+		/// parameters storage.
+		Mod::Par::Result fetch();
+		bool isValid() const;
+		bool trySetPassword(const std::string &password);
+		bool trySetSsid(const std::string &ssid);
+	};
 public:
 	Sta(esp_netif_t **);
 	void getFieldValue(Mod::Fld::Req aReq, Mod::Fld::OnResponseCallback aOnResponse);
-
+	void setFieldValue(Mod::Fld::WriteReq, Mod::Fld::OnWriteResponseCallback onResponse);
 private:
 	esp_netif_t **espNetif;
+	Credentials credentials;
 };
 
 }  // namespace Wifi
