9098d5e0a5534b183683bb8120c727b0fceae609
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-23 16:09:12 +0300

    [mav:FtpClient] Impl | Subscription on "BFT"

--- components/mav/priv_include/Microservice/FtpClient.cpp
+++ components/mav/priv_include/Microservice/FtpClient.cpp
@@ -10,12 +10,30 @@
 namespace Mav {
 namespace Mic {
 
+FtpClient::FtpClient():
+	subscriptionPackage{
+		{&FtpClient::onFileBufferingFinished, this}  // onFileBufferingFinished
+	}
+{
+}
+
+FtpClient::~FtpClient()
+{
+	subscriptionPackage.onFileBufferingFinished.setEnabled(false);
+}
+
 Microservice::Ret FtpClient::process(mavlink_message_t &aMessage, Microservice::OnResponseSignature aOnResponse)
 {
 	(void)aMessage;
 	(void)aOnResponse;
 
 	return Ret::Ignored;
+}
+
+void FtpClient::onFileBufferingFinished(std::shared_ptr<::Bft::File> aBftFile)
+{
+	(void)aBftFile;
+	// TODO:
 }
 
 }  // namespace Mic
--- components/mav/priv_include/Microservice/FtpClient.hpp
+++ components/mav/priv_include/Microservice/FtpClient.hpp
@@ -10,14 +10,29 @@
 
 #include "DelayedSend.hpp"
 #include "Microservice.hpp"
+#include "buffered_file_transfer/Sub.hpp"
 
 namespace Mav {
 namespace Mic {
 
+/// \brief Implements client side of MAVLink File Transfer Protocol for
+/// flashing file into AP's FS.
+class FtpClient final : public Microservice, public DelayedSend {
+private:
+	struct SubscriptionPackage {
+		Bft::OnFileBufferingFinished onFileBufferingFinished;
+	};
 
-class FtpClient : public Microservice, public DelayedSend {
 public:
+	FtpClient();
+	~FtpClient();
 	Ret process(mavlink_message_t &aMessage, OnResponseSignature aOnResponse) override;
+
+private:
+	void onFileBufferingFinished(std::shared_ptr<::Bft::File>);
+
+private:
+	SubscriptionPackage subscriptionPackage;
 };
 
 }  // namespace Mic
