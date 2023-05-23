2845d15c3a6429c5603f00f0794e34c643246198
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-23 17:44:04 +0300

    [mav:FtpClient] Impl | Request instantiation, request repeat (not ready)

--- components/mav/priv_include/Microservice/FtpClient.cpp
+++ components/mav/priv_include/Microservice/FtpClient.cpp
@@ -6,11 +6,22 @@
 //
 
 #include "FtpClient.hpp"
+#include "mav/mav.hpp"
+#include <esp_log.h>
 
 namespace Mav {
 namespace Mic {
 
+static constexpr const char *debugPreamble()
+{
+	return "FtpClient";
+}
+
+static constexpr std::chrono::milliseconds kRequestResendTimeout{200};
+static constexpr std::size_t knMaxAttempts = 4;
+
 FtpClient::FtpClient():
+	Ut::Sys::HrTimer{ESP_TIMER_TASK, "MavFtpClient"},
 	subscriptionPackage{
 		{&FtpClient::onFileBufferingFinished, this}  // onFileBufferingFinished
 	}
@@ -26,14 +37,62 @@ Microservice::Ret FtpClient::process(mavlink_message_t &aMessage, Microservice::
 {
 	(void)aMessage;
 	(void)aOnResponse;
+	// TODO
 
 	return Ret::Ignored;
 }
 
+void FtpClient::onHrTimer()
+{
+	std::lock_guard<std::mutex> lock{requestRepeat.mutex};
+
+	if (requestRepeat.stateCommon.iAttempt >= knMaxAttempts) {
+		ESP_LOGW(Mav::kDebugTag, "%s:%s: state \"%s\": exceeded the number of attempts", debugPreamble(), __func__,
+			requestRepeat.currentStateAsString());
+
+		// TODO: notify upon failure
+	} else {
+		++requestRepeat.stateCommon.iAttempt;
+	}
+
+	switch (requestRepeat.state) {
+		case RequestRepeat::StateIdle:
+			break;
+
+		case RequestRepeat::StateCreatingSession:
+			sendSessionOpenRequest();
+
+			break;
+
+		case RequestRepeat::StateTransferring:
+			sendFileTransferRequest();
+
+			break;
+	}
+
+	startOnce(kRequestResendTimeout);
+}
+
 void FtpClient::onFileBufferingFinished(std::shared_ptr<::Bft::File> aBftFile)
 {
-	(void)aBftFile;
-	// TODO:
+	std::lock_guard<std::mutex> lock{requestRepeat.mutex};
+
+	switch (requestRepeat.state) {
+		case RequestRepeat::StateIdle:
+			requestRepeat.stateCommon = {0, aBftFile};
+			sendSessionOpenRequest();  // XXX: from WQ?
+			// TODO: launch HrTimer
+
+			break;
+
+		default:
+			ESP_LOGW(Mav::kDebugTag, "%s:%s: already have an active session, ignoring", debugPreamble(), __func__);
+
+			break;
+	}
+
+	// TODO: register an attempt
+	// TODO: state
 }
 
 }  // namespace Mic
--- components/mav/priv_include/Microservice/FtpClient.hpp
+++ components/mav/priv_include/Microservice/FtpClient.hpp
@@ -11,16 +11,53 @@
 #include "DelayedSend.hpp"
 #include "Microservice.hpp"
 #include "buffered_file_transfer/Sub.hpp"
+#include "buffered_file_transfer/storage/File.hpp"
+#include "utility/system/HrTimer.hpp"
+#include <mutex>
 
 namespace Mav {
 namespace Mic {
 
 /// \brief Implements client side of MAVLink File Transfer Protocol for
 /// flashing file into AP's FS.
-class FtpClient final : public Microservice, public DelayedSend {
+class FtpClient final : public Microservice, public DelayedSend, public Ut::Sys::HrTimer {
 private:
 	struct SubscriptionPackage {
 		Bft::OnFileBufferingFinished onFileBufferingFinished;
+	};
+
+	/// \brief Keeps track of the previous attempts to send a file chunk to the
+	/// AP
+	struct RequestRepeat {
+		enum State {
+			StateIdle,
+			StateCreatingSession,
+			StateTransferring,
+		};
+
+		union {
+			struct {
+				std::size_t sessionId;
+			} stateCreatingSession;
+
+			struct {
+				std::size_t bftFilePosition;
+				std::size_t mavlinkFtpSessionId;
+			} stateTransferring;
+		};
+
+		/// \brief Entities used by more than 1 state
+		struct {
+			std::size_t iAttempt;
+			std::shared_ptr<Bft::File> bftFile;
+		} stateCommon;
+
+		std::mutex mutex;
+		State state;
+
+		const char *currentStateAsString();
+		// TODO: file position
+		// TOOD: BFT and HrTimer events come at different times
 	};
 
 public:
@@ -28,11 +65,21 @@ public:
 	~FtpClient();
 	Ret process(mavlink_message_t &aMessage, OnResponseSignature aOnResponse) override;
 
+	/// \brief Re-instantiates a request
+	void onHrTimer() override;
+
 private:
-	void onFileBufferingFinished(std::shared_ptr<::Bft::File>);
+	/// \brief Handles an "end-of-buffering" event
+	/// \param `aBftFile` is a RAII file wrapper. It is supposed to
+	/// automiatically close the file.
+	void onFileBufferingFinished(std::shared_ptr<::Bft::File> aBftFile);
+
+	void sendSessionOpenRequest();
+	void sendFileTransferRequest();
 
 private:
 	SubscriptionPackage subscriptionPackage;
+	RequestRepeat requestRepeat;
 };
 
 }  // namespace Mic
