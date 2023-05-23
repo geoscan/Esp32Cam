5e438cf56a0e70cdabafb3c785287ea12750ea60
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-23 20:14:07 +0300

    [mav:FileTransferProtocol] Impl | File session creation-related fields initializer

--- components/mav/priv_include/Helper/FileTransferProtocol.hpp
+++ components/mav/priv_include/Helper/FileTransferProtocol.hpp
@@ -9,8 +9,10 @@
 #define COMPONENTS_MAV_PRIV_INCLUDE_HELPER_FILETRANSFERPROTOCOL_HPP
 
 #include "Common.hpp"
+#include "Globals.hpp"
 #include "Mavlink.hpp"
-#include "Ftp/Types.hpp"
+#include "Microservice/Ftp/Types.hpp"
+#include <cstdint>
 
 namespace Mav {
 namespace Hlpr {
@@ -21,6 +23,21 @@ struct FileTransferProtocol : mavlink_file_transfer_protocol_t, Cmn::Impl::Pack<
 	inline Mav::Mic::Ftp::Payload &getPayload()
 	{
 		return *reinterpret_cast<Mav::Mic::Ftp::Payload *>(&payload);
+	}
+
+	/// \brief Fields related to creating a file session.
+	///	None = 0.
+	///	\param `aOperation`. Expected values are: `OpenFileRo`, `CreateFile`,
+	///	`OpenFileWo`
+	inline void setOpenFileSessionFields( std::uint16_t aSequenceNumber, Mic::Ftp::Op aOperation,
+		std::uint8_t aTargetSystem = Globals::getCompidAutopilot(),
+		std::uint8_t aTargetComponent = Globals::getSysId())
+	{
+		target_system = aTargetComponent;
+		target_component = aTargetComponent;
+		target_network = 0;
+		getPayload().seq_number = aSequenceNumber;
+		getPayload().opcode = aOperation;
 	}
 };
 
