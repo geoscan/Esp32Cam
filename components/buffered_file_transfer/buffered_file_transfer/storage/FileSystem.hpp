9facb0e55c6f28c81ccffab6752c07418f9b7784
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-24 13:39:33 +0300

    [buffered_file_transfer:FileSystem] Impl | `seek` and `read` APIs

--- components/buffered_file_transfer/buffered_file_transfer/storage/FileSystem.hpp
+++ components/buffered_file_transfer/buffered_file_transfer/storage/FileSystem.hpp
@@ -10,12 +10,21 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <stdio.h>
 
 namespace Bft {
 
 class FileDescriptor;
 
 class FileSystem {
+public:
+	enum Position : int {
+		PositionStart = SEEK_SET,
+		PositionCurrent = SEEK_CUR,
+		PositionEnd = SEEK_END,
+		PositionError = -4,
+	};
+
 public:
 	/// \brief Expected to open an existing file, or create a new one, if the
 	/// file with this name does not yet exist.
@@ -30,6 +39,14 @@ public:
 	/// \returns `aBufferSize`, if succeeded. 0 otherwise
 	virtual std::size_t append(FileDescriptor aFileDescriptor, const std::uint8_t *aBuffer,
 		std::size_t aBufferSize) = 0;
+
+	/// \brief Changes the file's current position
+	/// \returns The new position in a file (a positive integer) relative to
+	/// the origin, `PositionError` in case of an error.
+	virtual std::int32_t seek(FileDescriptor aFileDescriptor, std::int32_t aOffset, int aOrigin = PositionStart) = 0;
+
+	/// \returns the number of read bytes
+	virtual std::size_t read(FileDescriptor aFileDescriptor, std::uint8_t *aOutBuffer, std::size_t &aOutBufferSize) = 0;
 };
 
 }  // namespace Bft
