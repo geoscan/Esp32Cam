7bc2bdb00845259f45e2a29a8040d6e9970e5b23
DmitryMurashov <dmtr.murashov@gmail.com>
2023-05-25 16:48:38 +0300

    [buffered_file_transfer:Sub] Impl | Subscription topic

--- components/buffered_file_transfer/buffered_file_transfer/Sub.hpp
+++ components/buffered_file_transfer/buffered_file_transfer/Sub.hpp
@@ -20,6 +20,47 @@ namespace Bft {
 using OnFileBufferingFinished = Rr::Util::Key<void(std::shared_ptr<::Bft::File> aBftfile), typename ::Sub::MutSyncTrait,
 	std::list>;
 
+struct TransferUpdateEvent {
+	enum Status {
+		StatusInProgress,
+		StatusOk,
+		StatusFailed,
+	};
+
+	Status status;
+
+	union {
+		struct {
+			std::size_t currentOffset;
+			std::size_t fileSize;
+		} inProgressState;
+
+		struct {
+			const char *errorMessage;
+		} failedState;
+	};
+
+	inline TransferUpdateEvent &setInProgress(std::size_t aCurrentOffset, std::size_t aFileSize)
+	{
+		status = StatusInProgress;
+		inProgressState.currentOffset = aCurrentOffset;
+		inProgressState.fileSize = aFileSize;
+
+		return *this;
+	}
+
+	/// \pre `aErrorMessage` must have static lifetime
+	inline TransferUpdateEvent &setFailed(const char *aErrorMessage)
+	{
+		status = StatusFailed;
+		failedState.errorMessage = aErrorMessage;
+
+		return *this;
+	}
+};
+
+using OnTransferUpdate = Rr::Util::Key<void(TransferUpdateEvent), typename ::Sub::MutSyncTrait, std::list>;
+
 }  // namespace Bft
 
 #endif // COMPONENTS_BUFFERED_FILE_TRANSFER_BUFFERED_FILE_TRANSFER_SUB_HPP
