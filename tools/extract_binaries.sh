c0d2e61d30e2d97435a8a5c4d44cc5c8c1dbebdf
DmitryMurashov <dmtr.murashov@gmail.com>
2023-06-20 11:01:19 +0300

    [tools:extract_binaries] Enh | Grouping extracted binaries

--- tools/extract_binaries.sh
+++ tools/extract_binaries.sh
@@ -4,9 +4,10 @@ echo Removing...
 rm -f *.bin
 
 echo Extracting...
+mkdir firmware
 dir_build="./../build"
-cp $dir_build/esp32.bin .
-cp $dir_build/partition_table/partition-table.bin .
-cp $dir_build/bootloader/bootloader.bin .
+cp $dir_build/esp32.bin ./firmware
+cp $dir_build/partition_table/partition-table.bin ./firmware
+cp $dir_build/bootloader/bootloader.bin ./firmware
 
 
