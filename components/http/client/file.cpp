f03ce291584985f7bdba2d102a591977a61fa7aa
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-20 17:48:41 +0300

    [http:file] Impl | Handling various changes of HTTP exchange

--- components/http/client/file.cpp
+++ components/http/client/file.cpp
@@ -21,11 +21,61 @@
 static constexpr const char kHttpBufferSize = 128;
 static constexpr const char *kDebugContext = "http/file";
 
+/// \brief \sa `httpDownloadFileOverHttpGet`
+struct DownloadFileOverHttpContext {
+	/// The caller should provide the callback
+	OnFileChunkCallable callable;
+
+	static inline DownloadFileOverHttpContext &fromEspHttpClientEvent(esp_http_client_event_t *aEspHttpClientEvent)
+	{
+		return *static_cast<DownloadFileOverHttpContext *>(aEspHttpClientEvent->user_data);
+	}
+};
+
 static esp_err_t httpDownloadFileOverHttpGetEventHandler(esp_http_client_event_t *);
 
-static esp_err_t httpDownloadFileOverHttpGetEventHandler(esp_http_client_event_t *)
+static esp_err_t httpDownloadFileOverHttpGetEventHandler(esp_http_client_event_t *aEspHttpClientEvent)
 {
-	// TODO:
+	switch (aEspHttpClientEvent->event_id) {
+		case HTTP_EVENT_ON_FINISH:
+			DownloadFileOverHttpContext::fromEspHttpClientEvent(aEspHttpClientEvent).callable(nullptr, 0);  // The last chunk
+
+			break;
+
+		case HTTP_EVENT_ON_CONNECTED:
+			ESP_LOGI(kHttpDebugTag, "%s:%s: Successfully connected", kDebugContext, __func__);
+
+			break;
+
+		case HTTP_EVENT_ON_HEADER:
+
+			// Produce verbose output, if enabled
+#if CONFIG_HTTP_DEBUG_LEVEL == 5
+			ESP_LOGV(kHttpDebugTag, "%s:%s: Got header: \"%s\"=\"%s\"", kDebugContext, __func__, buffer,
+				aEspHttpClientEvent->header_key, aEspHttpClientEvent->header_value);
+#endif
+
+			break;
+
+		case HTTP_EVENT_ON_DATA:
+			DownloadFileOverHttpContext::fromEspHttpClientEvent(aEspHttpClientEvent).callable(
+				static_cast<const char *>(aEspHttpClientEvent->data), aEspHttpClientEvent->data_len);
+
+			break;
+
+		case HTTP_EVENT_DISCONNECTED:
+			ESP_LOGI(kHttpDebugTag, "%s:%s: disconnected from client", kDebugContext, __func__);
+
+			break;
+
+		case HTTP_EVENT_ERROR:
+			ESP_LOGE(kDebugContext, "%s:%s got error", kDebugContext, __func__);
+
+			break;
+
+		default:
+			break;
+	}
 
 	return ESP_FAIL;
 }
@@ -34,8 +84,10 @@ extern "C" esp_err_t httpDownloadFileOverHttpGet(const char *aFileUrl, OnFileChu
 {
 	// Init client
 	esp_http_client_config_t espHttpClientConfig{};
+	DownloadFileOverHttpContext downloadFileOverHttpContext {aOnFileChunkCallable};
 	espHttpClientConfig.url = aFileUrl;
 	espHttpClientConfig.event_handler = httpDownloadFileOverHttpGetEventHandler;
+	espHttpClientConfig.user_data = static_cast<void *>(&downloadFileOverHttpContext);
 	esp_http_client_handle_t espHttpClientHandle = esp_http_client_init(&espHttpClientConfig);
 
 	// Download file
