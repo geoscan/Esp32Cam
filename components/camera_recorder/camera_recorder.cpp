7a1b5eea913a4ac767c58bd3daa5030c9e7744a1
DmitryMurashov <dmtr.murashov@gmail.com>
2023-07-25 17:07:06 +0300

    [camera_recorder:camera_recorder] Impl | Static initialization of recorders

--- components/camera_recorder/camera_recorder.cpp
+++ components/camera_recorder/camera_recorder.cpp
@@ -9,8 +9,11 @@
 // Override debug level.
 // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html#_CPPv417esp_log_level_setPKc15esp_log_level_t
 #define LOG_LOCAL_LEVEL ((esp_log_level_t)CONFIG_CAMERA_RECORDER_DEBUG_LEVEL)
-#include "camera_recorder/camera_recorder.hpp"
+
+#include "camera_recorder/RecFrame.hpp"
+#include "camera_recorder/RecMjpgAvi.hpp"
 #include "camera_recorder/Storage.hpp"
+#include "camera_recorder/camera_recorder.hpp"
 
 namespace CameraRecorder {
 
@@ -19,6 +22,8 @@ const char *kDebugTag = "[camera_recorder]";
 void init()
 {
 #ifdef CONFIG_CAMERA_RECORDER_ENABLE
+	static RecFrame recFrame{};
+	static RecMjpgAvi recMjpgAvi{};
 	static Storage storage{};
 	(void)storage;
 	esp_log_level_set(CameraRecorder::kDebugTag, (esp_log_level_t)CONFIG_MAV_DEBUG_LEVEL);
