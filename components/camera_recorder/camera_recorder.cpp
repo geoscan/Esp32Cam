dcc404be296bf4757bebe904fda68feca5e56559
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-08 16:06:44 +0300

    [camera_recorder:camera_recorder] Impl | Integrated `StopRecordingWifiDisconnectHandler`

--- components/camera_recorder/camera_recorder.cpp
+++ components/camera_recorder/camera_recorder.cpp
@@ -5,13 +5,14 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
-#include <esp_log.h>
 // Override debug level.
 // https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/log.html#_CPPv417esp_log_level_setPKc15esp_log_level_t
 #define LOG_LOCAL_LEVEL ((esp_log_level_t)CONFIG_CAMERA_RECORDER_DEBUG_LEVEL)
+#include <esp_log.h>
 
 #include "camera_recorder/RecFrame.hpp"
 #include "camera_recorder/RecMjpgAvi.hpp"
+#include "camera_recorder/StopRecordingWifiDisconnectHandler.hpp"
 #include "camera_recorder/Storage.hpp"
 #include "camera_recorder/camera_recorder.hpp"
 
@@ -25,6 +26,7 @@ void init()
 	static RecFrame recFrame{};
 	static RecMjpgAvi recMjpgAvi{};
 	static Storage storage{};
+	static StopRecordingWifiDisconnectHandler stopRecordingWifiDisconnectHandler{&recMjpgAvi};
 	(void)storage;
 	esp_log_level_set(CameraRecorder::kDebugTag, (esp_log_level_t)CONFIG_MAV_DEBUG_LEVEL);
 	ESP_LOGD(CameraRecorder::kDebugTag, "Debug log test");
