f9b5ea6d9b8bea90ae01599be8d608dfd80edf99
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-09 11:43:36 +0300

    [buffered_file_transfer:Sub] Ref | Comments

--- components/buffered_file_transfer/buffered_file_transfer/Sub.hpp
+++ components/buffered_file_transfer/buffered_file_transfer/Sub.hpp
@@ -15,8 +15,9 @@
 
 namespace Bft {
 
-/// `aBftFile` is supposed to store a deleter which performs the cleanup. It
-/// implies that subscribers do not participate in file management.
+/// \brief Event key to notify subscribers upon buffering completion
+/// \details `aBftFile` is supposed to store a deleter which performs the
+/// cleanup. It implies that subscribers do not participate in file management.
 using OnFileBufferingFinished = Rr::Util::Key<void(std::shared_ptr<::Bft::File> aBftfile), typename ::Sub::MutSyncTrait,
 	std::list>;
 
@@ -66,6 +67,10 @@ struct TransferUpdateEvent {
 	}
 };
 
+/// \brief Notifies subscribers upon transferring the file into the storage (be
+/// that a directly wired SPI storage, or AP which is communicated w/ over a
+/// serial interface). In the latter case, `Mav::FtpClient` issues those events
+/// at various stages of the transfer.
 using OnTransferUpdate = Rr::Util::Key<void(TransferUpdateEvent), typename ::Sub::MutSyncTrait, std::list>;
 
 }  // namespace Bft
