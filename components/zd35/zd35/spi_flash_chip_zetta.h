7054135d6465e1d92802b20e4602a18bdfec9c67
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-23 14:29:04 +0300

    [zd35:spi_flash_chip_zetta] Impl |

---
+++ components/zd35/zd35/spi_flash_chip_zetta.h
@@ -0,0 +1,24 @@
+//
+// spi_flash_chip_zetta.h
+//
+// Created on: Aug 23, 2023
+//     Author: Dmitry Murashov (d <DOT> murashov <AT> geoscan <DOT> aero)
+//
+
+
+#ifndef COMPONENTS_ZD35_ZD35_SPI_FLASH_CHIP_ZETTA_H
+#define COMPONENTS_ZD35_ZD35_SPI_FLASH_CHIP_ZETTA_H
+
+#include <esp_flash.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif  // __cplusplus
+
+extern const spi_flash_chip_t esp_flash_chip_zetta;
+
+#ifdef __cplusplus
+}
+#endif  // __cplusplus
+
+#endif // COMPONENTS_ZD35_ZD35_SPI_FLASH_CHIP_ZETTA_H
