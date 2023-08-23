3789faf0f050467aaa57b7d6b03e86f6e3773a04
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-23 17:50:07 +0300

    [zd35:zd35_defs] Impl |

---
+++ components/zd35/zd35/zd35_defs.hpp
@@ -0,0 +1,18 @@
+//
+// zd35_defs.hpp
+//
+// Created on: Aug 23, 2023
+//     Author: Dmitry Murashov (dmtr <DOT> murashov <AT> <GMAIL> <DOT> <COM>)
+//
+
+#ifndef COMPONENTS_ZD35_ZD35_DEFS_HPP
+#define COMPONENTS_ZD35_ZD35_DEFS_HPP
+
+enum {
+	Zd35x2PageSize = 2176,
+	Zd35x2BlockSize = 2 * Zd35x2PageSize,
+	Zd35x2ChipId = 0x2C24,
+	Zd35x2BlocksNumber = 1024 * 2,  // 2 panes x 1024 blocks
+};
+
+#endif // COMPONENTS_ZD35_ZD35_DEFS_HPP
