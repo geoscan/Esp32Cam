7ba6fa4267f7397a2e414f62b17598ea01e24e9a
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-24 16:07:12 +0300

    [zd35:zd35_defs] Impl | Overall 2Gb version chip size

--- components/zd35/zd35/zd35_defs.hpp
+++ components/zd35/zd35/zd35_defs.hpp
@@ -13,6 +13,7 @@ enum {
 	Zd35x2BlockSize = 64 * Zd35x2PageSize,  // unit: bytes
 	Zd35x2ChipId = 0x2C24,
 	Zd35x2BlocksNumber = 1024 * 2,  // 2 panes x 1024 blocks
+	Zd35x2CapacityBytes = Zd35x2BlocksNumber * Zd35x2BlockSize,
 };
 
 #endif // COMPONENTS_ZD35_ZD35_DEFS_HPP
