732c3969b3b0df57aa4875963356773cab78925b
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-25 18:23:13 +0300

    [zd35:zd35_defs] Impl | Additional constants

--- components/zd35/zd35/zd35_defs.hpp
+++ components/zd35/zd35/zd35_defs.hpp
@@ -8,12 +8,32 @@
 #ifndef COMPONENTS_ZD35_ZD35_DEFS_HPP
 #define COMPONENTS_ZD35_ZD35_DEFS_HPP
 
+// Memory layout
+
 enum {
 	Zd35x2PageSize = 2176,  // unit: bytes
 	Zd35x2BlockSize = 64 * Zd35x2PageSize,  // unit: bytes
 	Zd35x2ChipId = 0x2C24,
 	Zd35x2BlocksNumber = 1024 * 2,  // 2 panes x 1024 blocks
 	Zd35x2CapacityBytes = Zd35x2BlocksNumber * Zd35x2BlockSize,
+};
+
+// Commands
+
+enum Zd35Command {
+	Zd35CommandGetFeatures = 0x0F,
+};
+
+// Registers
+
+enum Zd35Address {
+	Zd35AddressBlockLock = 0xA0,
+};
+
+// Bits
+
+enum Zd35Register {
+	Zd35RegisterBlockLockBrwdMask = 7  // 8-bit BRWD register
 };
 
 #endif // COMPONENTS_ZD35_ZD35_DEFS_HPP
