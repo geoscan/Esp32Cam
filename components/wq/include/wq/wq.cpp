eeed8d3d285f1f223ad9fb69d16f5e133d505c20
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-30 12:30:32 +0300

    [wq:wq] Impl | Integrated "Sys::WorkQueue" API implementation

--- components/wq/include/wq/wq.cpp
+++ components/wq/include/wq/wq.cpp
@@ -5,14 +5,19 @@
 //     Author: Dmitry Murashov (d.murashov@geoscan.aero)
 //
 
-#include "wq.hpp"
+#include "utility/MakeSingleton.hpp"
 #include "utility/thr/WorkQueue.hpp"
+#include "wq/PortWorkQueue.hpp"
+
+#include "wq.hpp"
 
 namespace Wq {
 
 void start()
 {
-	static Ut::Thr::Wq::MediumPriority wqMediumPriority;
+	static Ut::Thr::Wq::MediumPriority wqMediumPriority{};
+	static Wq::PortWorkQueue portWorkQueue{};
+	Ut::MakeSingleton<Sys::WorkQueue>::setInstance(portWorkQueue);
 	wqMediumPriority.start();
 }
 
