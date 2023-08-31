dda9885342697fba77f46cffe902ec1c66464a96
DmitryMurashov <dmtr.murashov@gmail.com>
2023-08-31 10:04:24 +0300

    [zd35:zd35_defs] Impl | Registers, addresses, memory layout specs

--- components/zd35/zd35/zd35_defs.hpp
+++ components/zd35/zd35/zd35_defs.hpp
@@ -12,6 +12,7 @@
 
 enum {
 	Zd35x2PageSize = 2176,  // unit: bytes
+	Zd35x2CacheSize = Zd35x2PageSize,
 	Zd35x2BlockSize = 64 * Zd35x2PageSize,  // unit: bytes
 	Zd35x2ChipId = 0x2C24,
 	Zd35x2BlocksNumber = 1024 * 2,  // 2 panes x 1024 blocks
@@ -23,12 +24,15 @@ enum {
 enum Zd35Command {
 	Zd35CommandGetFeatures = 0x0F,
 	Zd35CommandSetFeatures = 0x1F,
+	Zd35CommandPageRead = 0x13,  // Read page into cache
+	Zd35CommandReadFromCache = 0x03,
 };
 
 // Registers
 
 enum Zd35Address {
 	Zd35AddressBlockLock = 0xA0,
+	Zd35AddressStatus = 0xC0,
 };
 
 // Bits
@@ -40,6 +44,7 @@ enum Zd35Register {
 	Zd35registerBlockLockBp1 = 1 << 4,
 	Zd35registerBlockLockBp0 = 1 << 3,
 	Zd35registerBlockLockTb = 1 << 2,
+	Zd35RegisterStatusOip = 1 << 0,  // Operation in progress
 };
 
 #endif // COMPONENTS_ZD35_ZD35_DEFS_HPP
