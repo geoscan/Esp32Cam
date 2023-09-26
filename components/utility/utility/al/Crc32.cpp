849c8dd23737648e71694660681faee743596344
DmitryMurashov <dmtr.murashov@gmail.com>
2023-09-26 17:43:51 +0300

    [utility:Crc32] Ref | STL u32

--- components/utility/utility/al/Crc32.cpp
+++ components/utility/utility/al/Crc32.cpp
@@ -10,7 +10,7 @@
 namespace Ut {
 namespace Al {
 
-uint32_t Crc32::updateCrc32(uint32_t cs, const void *buffer, uint32_t len) {
+std::uint32_t Crc32::updateCrc32(uint32_t cs, const void *buffer, uint32_t len) {
 	static const uint32_t table[256] = {
 		0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA,
 		0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
--- components/utility/utility/al/Crc32.hpp
+++ components/utility/utility/al/Crc32.hpp
@@ -8,17 +8,18 @@
 #ifndef COMPONENTS_UTILITY_UTILITY_AL_CRC32_HPP
 #define COMPONENTS_UTILITY_UTILITY_AL_CRC32_HPP
 
-#include <stdint.h>
+#include <cstdint>
 
 namespace Ut {
 namespace Al {
 
 struct Crc32 {
-	static uint32_t calculateCrc32(const void *data, uint32_t len) {
+	static std::uint32_t calculateCrc32(const void *data, std::uint32_t len)
+	{
 		return updateCrc32(0, data, len);
 	}
 
-	static uint32_t updateCrc32(uint32_t cs, const void *buffer, uint32_t len);
+	static std::uint32_t updateCrc32(uint32_t cs, const void *buffer, std::uint32_t len);
 };
 
 }  // Al
