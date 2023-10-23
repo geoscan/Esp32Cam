2ff8139dee0eef4a5330c0bfce2a9ce9008f8a04
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-23 13:22:49 +0300

    [buffered_file_transfer:SaluteFlashMemoryTransferImplementor] Ref | Extended functionality description

--- components/buffered_file_transfer/buffered_file_transfer/process/SaluteFlashMemoryTransferImplementor.hpp
+++ components/buffered_file_transfer/buffered_file_transfer/process/SaluteFlashMemoryTransferImplementor.hpp
@@ -13,7 +13,16 @@
 namespace Bft {
 
 /// \brief Performs memory management taking account of the memory management
-/// details specific to Salute drones
+/// details specific to Salute drones.
+///
+/// \details Ensures that SPI flash memory format complies with the following
+/// structure:
+/// `[file size 4 bytes][file payload N bytes]`.
+/// This format is maintained in the following way:
+/// - `onFileBufferingFinishedPreBufferRead(...)` ensures 4 byte padding before
+///   writing
+/// - `onFileBufferingFinishedPostChunkFlushed(...)` rewrites the first page,
+///   placing the file's size in the first 4 bytes
 class SaluteFlashMemoryTransferImplementor : public FlashMemoryTransferImplementor {
 public:
 	using FlashMemoryTransferImplementor::FlashMemoryTransferImplementor;
