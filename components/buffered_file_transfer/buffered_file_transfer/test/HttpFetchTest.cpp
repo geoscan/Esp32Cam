94615c899aa1080792d836adad3bc4a57b2a5f1a
DmitryMurashov <dmtr.murashov@gmail.com>
2023-10-25 14:29:03 +0300

    [buffered_file_transfer:HttpFetchTest] Ref | Removed duplicating code, since it is already initialized in "HttpFetch"

--- components/buffered_file_transfer/buffered_file_transfer/test/HttpFetchTest.cpp
+++ components/buffered_file_transfer/buffered_file_transfer/test/HttpFetchTest.cpp
@@ -18,13 +18,6 @@ namespace Bft {
 
 static constexpr const char *kLogPreamble = "HttpFetchTest";
 
-int HttpFetchTest::onFileChunkReceivedWrapper(const char *aChunk, size_t aChunkSize, void *aUserData)
-{
-	static_cast<HttpFetchTest *>(aUserData)->onFileChunkReceived(aChunk, aChunkSize);
-
-	return ESP_OK;
-}
-
 HttpFetchTest::HttpFetchTest(const char *aFileHttpUrl, const char *aBufferedFileTransferFileName):
 	fileHttpUrl{aFileHttpUrl},
 	bufferedFileTransferFileName{aBufferedFileTransferFileName}
@@ -33,64 +26,9 @@ HttpFetchTest::HttpFetchTest(const char *aFileHttpUrl, const char *aBufferedFile
 
 void HttpFetchTest::runTest()
 {
-	Sys::Logger::write(Sys::LogLevel::Debug, debugTag(), "%s:%s", kLogPreamble, __func__);
-	if (!BufferedFileTransfer::checkInstance()) {
-		Sys::Logger::write(Sys::LogLevel::Error, debugTag(),
-			"%s:%s `BufferedFileTransfer` instance has not been initialized, panicking...", kLogPreamble, __func__);
-		Sys::panic();
-	}
-
-	static constexpr std::size_t kBufferSizeHint = 20 * 1024;
-	bftFile = std::shared_ptr<Bft::File>{new Bft::File{BufferedFileTransfer::getInstance().tryOpenFileWriteBinary(
-		bufferedFileTransferFileName, kBufferSizeHint)},
-		// deleter
-		[](Bft::File *aFile) mutable
-		{
-			aFile->close();
-			delete aFile;
-		}};
-
-	if (!bftFile->isValid()) {
-		Sys::Logger::write(Sys::LogLevel::Error, debugTag(), "%s:%s Failed to open file %s", kLogPreamble, __func__,
-			bufferedFileTransferFileName);
-		Sys::panic();
-	}
-
-	Sys::Logger::write(Sys::LogLevel::Debug, debugTag(), "%s:%s starting file acquisition", kLogPreamble, __func__);
-	const auto fileGetResult = httpDownloadFileOverHttpGetByUrl(fileHttpUrl, onFileChunkReceivedWrapper,
-		static_cast<void *>(this));
-
-	if (fileGetResult != ESP_OK) {
-		Sys::Logger::write(Sys::LogLevel::Warning, debugTag(), "%s:%s failed to retrieve a file", kLogPreamble,
-			__func__);
-	}
-
-	bftFile.reset();
-}
-
-void HttpFetchTest::onFileChunkReceived(const char *aChunk, size_t aChunkSize)
-{
-	Sys::Logger::write(Sys::LogLevel::Verbose, debugTag(), "%s:%s got chunk, is nullptr = %d, chunk size = %d",
-		kLogPreamble, __func__, static_cast<int>(aChunk == nullptr), static_cast<int>(aChunkSize));
-
-	if (aChunk == nullptr && aChunkSize != 0) {  // Announcing file size
-		Sys::Logger::write(Sys::LogLevel::Info, debugTag(), "%s:%s receiving file %d bytes", kLogPreamble, __func__,
-			aChunkSize);
-	} else if (aChunk == nullptr && aChunkSize == 0) {  // Finalizing reception
-		TransferImplementor::notifyAllOnFileBufferingFinished(bftFile, true);
-		Sys::Logger::write(Sys::LogLevel::Info, debugTag(), "%s:%s finalizing write", kLogPreamble, __func__);
-	} else if (aChunk != nullptr && aChunkSize != 0) {  // Handling new chunk
-		const auto nAppended = bftFile->append(reinterpret_cast<const std::uint8_t *>(aChunk), aChunkSize);
-
-		if (nAppended != aChunkSize) {
-			Sys::Logger::write(Sys::LogLevel::Warning, debugTag(),
-				"%s:%s failed to bufferize %d bytes, managed to bufferize %d instead", kLogPreamble, __func__,
-				aChunkSize, nAppended);
-		}
-	} else {
-		Sys::Logger::write(Sys::LogLevel::Warning, debugTag(), "%s:%s \"File\" API unexpected case, ignoring",
-			kLogPreamble, __func__);
-	}
+	Sys::Logger::write(Sys::LogLevel::Info, debugTag(),
+		"%s:%s initializing file transfer name=\"%s\" BFT file name=\"%s\"", kLogPreamble, __func__);
+	fetchFile(fileHttpUrl, bufferedFileTransferFileName);
 }
 
 }  // Bft
--- components/buffered_file_transfer/buffered_file_transfer/test/HttpFetchTest.hpp
+++ components/buffered_file_transfer/buffered_file_transfer/test/HttpFetchTest.hpp
@@ -9,13 +9,14 @@
 #define COMPONENTS_BUFFERED_FILE_TRANSFER_BUFFERED_FILE_TRANSFER_HTTPFETCHTEST_HPP
 
 #include "buffered_file_transfer/BufferedFileTransfer.hpp"
+#include "buffered_file_transfer/process/HttpFetch.hpp"
 #include <memory>
 
 namespace Bft {
 
 /// \brief Implements fetching of a file over HTTP, and its further transfer
 /// using whatever scheme happens to be employed at a time
-class HttpFetchTest {
+class HttpFetchTest : public HttpFetch {
 public:
 	/// \brief aFileHttpUrl URL which can be used to retrieve the file
 	/// \brief aBufferedFileTransferFileName File that will be references when
@@ -23,19 +24,9 @@ public:
 	HttpFetchTest(const char *aFileHttpUrl, const char *aBufferedFileTransferFileName);
 
 	void runTest();
-
-private:
-	/// \pre `bftFile` is valid
-	void onFileChunkReceived(const char *aChunk, size_t aChunkSize);
-
-	/// \brief Callback. The signature is compliant with HTTP file API (see
-	/// `file.h`)
-	static int onFileChunkReceivedWrapper(const char *aChunk, size_t aChunkSize, void *aUserData);
-
 private:
 	const char *fileHttpUrl;
 	const char *bufferedFileTransferFileName;
-	std::shared_ptr<Bft::File> bftFile;
 };
 
 }  // Bft
